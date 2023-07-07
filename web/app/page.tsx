12199bcfa85cac6854953381976dc9956b4c0cf7
Jeffrey Morgan <jmorganca@gmail.com>
2023-07-07 15:21:40 -0400

    update download links to the releases page until we have a better download url

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -5,18 +5,15 @@ export default async function Home() {
     <main className='flex min-h-screen max-w-2xl flex-col p-4 lg:p-24'>
       <h1 className='font-serif text-3xl'>ollama</h1>
       <section className='my-8'>
-        <p className='my-3 mb-8 max-w-md'>
+        <p className='my-3 max-w-md'>
           <a className='underline' href='https://github.com/jmorganca/ollama'>
             Ollama
           </a>{' '}
-          is a tool for running large language models.
-          <br />
-          <br />
-          Get started with Ollama using pip:
+          is a tool for running large language models. The latest version is available for download{' '}
+          <a className='underline' href='https://github.com/jmorganca/ollama/releases/latest'>
+            here.
+          </a>
         </p>
-        <pre className='my-4'>
-          <code>pip install ollama</code>
-        </pre>
       </section>
       <section className='my-4'>
         <h2 className='mb-4 text-lg'>Example models you can try running:</h2>
