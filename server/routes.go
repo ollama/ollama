5c26b81a2f4b9b77064e32e380713217fc47f69b
Patrick Devine <patrick@infrahq.com>
2023-07-18 12:37:51 -0700

    skip files in the list if we can't get the correct model path

--- server/routes.go
+++ server/routes.go
@@ -206,8 +206,8 @@ func list(c *gin.Context) {
 			mp := ParseModelPath(tag)
 			manifest, err := GetManifest(mp)
 			if err != nil {
-				log.Printf("couldn't get manifest: %v", err)
-				return err
+				log.Printf("skipping file: %s", fp)
+				return nil
 			}
 			model := api.ListResponseModel{
 				Name:       mp.GetShortTagname(),
