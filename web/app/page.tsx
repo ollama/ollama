3c8f4c03d7b3f1132ce9a5fd4e42597dac7b2ea4
Jeffrey Morgan <jmorganca@gmail.com>
2023-07-21 09:57:57 -0700

    web: tweak homepage text

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -11,18 +11,23 @@ export default async function Home() {
         <Image src='/ollama.png' width={64} height={64} alt='ollamaIcon' />
         <section className='my-12 text-center'>
           <div className='flex flex-col space-y-2'>
-            <h2 className='md:max-w-[18rem] mx-auto my-2 text-3xl tracking-tight'>Portable large language models</h2>
+            <h2 className='md:max-w-md mx-auto my-2 text-3xl tracking-tight'>
+              Get up and running with large language models, locally.
+            </h2>
             <h3 className='md:max-w-xs mx-auto text-base text-neutral-500'>
-              Bundle a model’s weights, configuration, prompts, data and more into self-contained packages that run anywhere.
+              Run Llama 2 and other models on macOS. Customize and create your own.
             </h3>
           </div>
-          <div className='mx-auto flex flex-col space-y-4 mt-12'>
-            <Link href='/download' className='md:mx-10 lg:mx-14 bg-black text-white rounded-full px-4 py-2 focus:outline-none cursor-pointer'>
+          <div className='mx-auto max-w-xs flex flex-col space-y-4 mt-12'>
+            <Link
+              href='/download'
+              className='md:mx-10 lg:mx-14 bg-black text-white rounded-full px-4 py-2 focus:outline-none cursor-pointer'
+            >
               Download
             </Link>
             <p className='text-neutral-500 text-sm '>
-            Available for macOS with Apple Silicon <br />
-            Windows & Linux support coming soon.
+              Available for macOS with Apple Silicon <br />
+              Windows & Linux support coming soon.
             </p>
           </div>
         </section>
