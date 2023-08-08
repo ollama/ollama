a027a7dd65bb5f30ceabc45513956b4994581b92
Jeffrey Morgan <jmorganca@gmail.com>
2023-08-08 13:39:50 -0700

    add `0.0.0.0` as an allowed origin by default
    Fixes #282

--- server/routes.go
+++ server/routes.go
@@ -323,7 +323,6 @@ func CopyModelHandler(c *gin.Context) {
 func Serve(ln net.Listener, extraOrigins []string) error {
 	config := cors.DefaultConfig()
 	config.AllowWildcard = true
-	// only allow http/https from localhost
 	allowedOrigins := []string{
 		"http://localhost",
 		"http://localhost:*",
@@ -333,6 +332,10 @@ func Serve(ln net.Listener, extraOrigins []string) error {
 		"http://127.0.0.1:*",
 		"https://127.0.0.1",
 		"https://127.0.0.1:*",
+		"http://0.0.0.0",
+		"http://0.0.0.0:*",
+		"https://0.0.0.0",
+		"https://0.0.0.0:*",
 	}
 	allowedOrigins = append(allowedOrigins, extraOrigins...)
 	config.AllowOrigins = allowedOrigins
