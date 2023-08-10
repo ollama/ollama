4ab1da38ba2b061b0f2146e9f15568d791845466
Jeffrey Morgan <jmorganca@gmail.com>
2023-08-10 14:11:54 -0700

    guard around `id()`

--- app/src/index.ts
+++ app/src/index.ts
@@ -236,7 +236,11 @@ app.on('window-all-closed', () => {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
-const aid = id()
+let aid = ''
+try {
+  aid = id()
+} catch (e) {}
+
 autoUpdater.setFeedURL({
   url: `https://ollama.ai/api/update?os=${process.platform}&arch=${process.arch}&version=${app.getVersion()}&id=${aid}`,
 })
