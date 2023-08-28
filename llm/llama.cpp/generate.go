eb977744e18710e422e915c31137b6561de6784e
Bruce MacDonald <brucewmacdonald@gmail.com>
2023-08-28 14:26:44 -0400

    update submodule command

--- llm/llama.cpp/generate.go
+++ llm/llama.cpp/generate.go
@@ -1,6 +1,7 @@
 package llm
 
-//go:generate git submodule update --init --recursive
+//go:generate git submodule init
+//go:generate git submodule update ggml
 //go:generate sh ggml_patch/apply.sh
 //go:generate cmake -S ggml -B ggml/build/cpu
 //go:generate cmake --build ggml/build/cpu --target server --config Release
--- llm/llama.cpp/generate_darwin.go
+++ llm/llama.cpp/generate_darwin.go
@@ -3,7 +3,8 @@
 
 package llm
 
-//go:generate git submodule update --init --recursive
+//go:generate git submodule init
+//go:generate git submodule update ggml
 //go:generate sh ggml_patch/apply.sh
 //go:generate cmake -S ggml -B ggml/build/gpu -DLLAMA_METAL=on -DLLAMA_K_QUANTS=on
 //go:generate cmake --build ggml/build/gpu --target server --config Release
