7fa6e51686236315ee4920dc79ceb1d7ef3d8199
Jeffrey Morgan <jmorganca@gmail.com>
2023-09-05 12:53:57 -0400

    generate binary dependencies based on GOARCH on macos (#459)

--- llm/llama.cpp/generate_darwin_arm64.go
+++ llm/llama.cpp/generate_darwin_arm64.go
@@ -1,11 +1,8 @@
-//go:build !darwin
-// +build !darwin
-
 package llm
 
 //go:generate git submodule init
 //go:generate git submodule update --force ggml
 //go:generate git -C ggml apply ../ggml_patch/0001-add-detokenize-endpoint.patch
 //go:generate git -C ggml apply ../ggml_patch/0002-34B-model-support.patch
-//go:generate cmake -S ggml -B ggml/build/cpu -DLLAMA_K_QUANTS=on
-//go:generate cmake --build ggml/build/cpu --target server --config Release
+//go:generate cmake -S ggml -B ggml/build/gpu -DLLAMA_METAL=on -DLLAMA_ACCELERATE=on -DLLAMA_K_QUANTS=on -DCMAKE_SYSTEM_PROCESSOR=arm64 -DCMAKE_OSX_ARCHITECTURES=arm64
+//go:generate cmake --build ggml/build/gpu --target server --config Release
