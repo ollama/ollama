e95bc890ecad3932d5807c0a8d74710370569732
Bruce MacDonald <brucewmacdonald@gmail.com>
2023-09-05 14:20:48 -0400

    linux gpu support

---
+++ llm/llama.cpp/generate_linux.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# only do gpu builds if nvcc is installed
+if which nvcc > /dev/null; then
+  cmake --fresh -S ggml -B ggml/build/cpu -DLLAMA_CUBLAS=on -DLLAMA_ACCELERATE=on -DLLAMA_K_QUANTS=on
+  cmake --build ggml/build/cpu --target server --config Release
+
+  cmake --fresh -S gguf -B gguf/build/gpu -DLLAMA_CUBLAS=on -DLLAMA_ACCELERATE=on -DLLAMA_K_QUANTS=on
+  cmake --build gguf/build/gpu --target server --config Release
+else
+  echo "Warning: nvcc is not installed, skipping gpu builds."
+fi
