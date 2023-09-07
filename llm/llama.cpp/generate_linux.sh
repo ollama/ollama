66061920e0327a946ccbde18e1481b53a2f78812
Bruce MacDonald <brucewmacdonald@gmail.com>
2023-09-07 17:55:04 -0400

    Update generate_linux.sh

--- llm/llama.cpp/generate_linux.sh
+++ llm/llama.cpp/generate_linux.sh
@@ -2,8 +2,8 @@
 
 # only do gpu builds if nvcc is installed
 if which nvcc > /dev/null; then
-  cmake --fresh -S ggml -B ggml/build/cpu -DLLAMA_CUBLAS=on -DLLAMA_ACCELERATE=on -DLLAMA_K_QUANTS=on
-  cmake --build ggml/build/cpu --target server --config Release
+  cmake --fresh -S ggml -B ggml/build/gpu -DLLAMA_CUBLAS=on -DLLAMA_ACCELERATE=on -DLLAMA_K_QUANTS=on
+  cmake --build ggml/build/gpu --target server --config Release
 
   cmake --fresh -S gguf -B gguf/build/gpu -DLLAMA_CUBLAS=on -DLLAMA_ACCELERATE=on -DLLAMA_K_QUANTS=on
   cmake --build gguf/build/gpu --target server --config Release
