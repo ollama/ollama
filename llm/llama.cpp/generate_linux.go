ce5cc397b9681bafcec23016e8084a8dc4db9142
Bruce MacDonald <brucewmacdonald@gmail.com>
2023-09-12 13:44:10 -0400

    cpu builds

--- llm/llama.cpp/generate_linux.go
+++ llm/llama.cpp/generate_linux.go
@@ -6,7 +6,11 @@ package llm
 //go:generate -command git-apply git -C ggml apply
 //go:generate git-apply ../ggml_patch/0001-add-detokenize-endpoint.patch
 //go:generate git-apply ../ggml_patch/0002-34B-model-support.patch
+//go:generate cmake -S ggml -B ggml/build/cpu -DLLAMA_K_QUANTS=on
+//go:generate cmake --build ggml/build/cpu --target server --config Release
 
 //go:generate git submodule update --force gguf
+//go:generate cmake -S gguf -B gguf/build/cpu -DLLAMA_K_QUANTS=on
+//go:generate cmake --build gguf/build/cpu --target server --config Release
 
 //go:generate sh generate_linux.sh
