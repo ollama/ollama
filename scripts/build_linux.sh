f6cbfa5afcef996cb54d1d466134b0f11711dc9f
Jeffrey Morgan <jmorganca@gmail.com>
2023-09-20 21:38:35 -0400

    fix go compile

--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+set -e
+
 mkdir -p dist
 
-docker build --platform=linux/amd64 -f Dockerfile.build . -t builder
-docker create --platform linux/amd64 --name builder builder
+docker buildx build --platform=linux/amd64 -f Dockerfile.build . -t builder:amd64 --load
+docker create --platform linux/amd64 --name builder builder:amd64
 docker cp builder:/go/src/github.com/jmorganca/ollama/ollama ./dist/ollama-linux-amd64
 docker rm builder
 
-docker build --platform=linux/arm64 -f Dockerfile.build . -t builder
-docker create --platform linux/arm64 --name builder builder
+docker buildx build --platform=linux/arm64 -f Dockerfile.build . -t builder:arm64 --load
+docker create --platform linux/arm64 --name builder builder:arm64
 docker cp builder:/go/src/github.com/jmorganca/ollama/ollama ./dist/ollama-linux-arm64
 docker rm builder
