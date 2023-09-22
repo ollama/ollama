4b79ea9edfb683274aaf7b1a4ff6f3efa0c1e4d6
Jeffrey Morgan <jmorganca@gmail.com>
2023-09-22 12:51:12 -0400

    Update scripts/build_linux.sh
    Co-authored-by: Michael Yang <mxyng@pm.me>

--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -4,12 +4,9 @@ set -e
 
 mkdir -p dist
 
-docker buildx build --platform=linux/amd64 -f Dockerfile.build . -t builder:amd64 --load
-docker create --platform linux/amd64 --name builder builder:amd64
-docker cp builder:/go/src/github.com/jmorganca/ollama/ollama ./dist/ollama-linux-amd64
-docker rm builder
-
-docker buildx build --platform=linux/arm64 -f Dockerfile.build . -t builder:arm64 --load
-docker create --platform linux/arm64 --name builder builder:arm64
-docker cp builder:/go/src/github.com/jmorganca/ollama/ollama ./dist/ollama-linux-arm64
-docker rm builder
+for ARCH in arm64 amd64; do
+    docker buildx build --platform=linux/$ARCH -f Dockerfile.build . -t builder:$ARCH --load
+    docker create --platform linux/$ARCH --name builder builder:$ARCH
+    docker cp builder:/go/src/github.com/jmorganca/ollama/ollama ./dist/ollama-linux-$ARCH
+    docker rm builder
+done
