2b15d10aaa209a4c766134936f92a41e116b0d33
Bruce MacDonald <brucewmacdonald@gmail.com>
2023-09-28 16:14:34 -0400

    add cuda to windows generation

--- llm/llama.cpp/generate_windows.go
+++ llm/llama.cpp/generate_windows.go
@@ -12,3 +12,8 @@ package llm
 //go:generate git -C gguf apply ../patches/0001-remove-warm-up-logging.patch
 //go:generate cmake -S gguf -B gguf/build/cpu -DLLAMA_K_QUANTS=on
 //go:generate cmake --build gguf/build/cpu --target server --config Release
+
+//go:generate cmake -S ggml -B ggml/build/cuda -DLLAMA_CUBLAS=on -DLLAMA_ACCELERATE=on -DLLAMA_K_QUANTS=on
+//go:generate cmake --build ggml/build/cuda --target server --config Release
+//go:generate cmake -S gguf -B gguf/build/cuda -DLLAMA_CUBLAS=on -DLLAMA_ACCELERATE=on -DLLAMA_K_QUANTS=on
+//go:generate cmake --build gguf/build/cuda --target server --config Release
