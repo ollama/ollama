1ebdbd9694de0746dd0860ffb88f25559794665b
Michael Yang <mxyng@pm.me>
2023-10-12 15:45:07 -0700

    server: add version handler

--- server/routes.go
+++ server/routes.go
@@ -782,6 +782,9 @@ func Serve(ln net.Listener, allowOrigins []string) error {
 		})
 
 		r.Handle(method, "/api/tags", ListModelsHandler)
+		r.Handle(method, "/api/version", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{"version": version.Version})
+		})
 	}
 
 	log.Printf("Listening on %s (version %s)", ln.Addr(), version.Version)
