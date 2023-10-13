832b4db9d4baf22497145dde55f334b292ed665f
Jeffrey Morgan <jmorganca@gmail.com>
2023-10-13 19:04:42 -0400

    Use correct url for auto updates

--- app/src/index.ts
+++ app/src/index.ts
@@ -162,11 +162,14 @@ app.on('before-quit', () => {
   }
 })
 
-let currentReleaseURL = ''
+const updateURL = `https://ollama.ai/api/update?os=${process.platform}&arch=${
+  process.arch
+}&version=${app.getVersion()}&id=${id()}`
 
+let latest = ''
 async function isNewReleaseAvailable() {
   try {
-    const response = await fetch('https://ollama.ai/api/update')
+    const response = await fetch(updateURL)
 
     if (!response.ok) {
       return false
@@ -183,11 +186,12 @@ async function isNewReleaseAvailable() {
       return false
     }
 
-    if (url === currentReleaseURL) {
+    if (latest === url) {
       return false
     }
 
-    currentReleaseURL = url
+    latest = url
+
     return true
   } catch (error) {
     logger.error(`update check failed - ${error}`)
@@ -285,11 +289,7 @@ function id(): string {
   return uuid
 }
 
-autoUpdater.setFeedURL({
-  url: `https://ollama.ai/api/update?os=${process.platform}&arch=${
-    process.arch
-  }&version=${app.getVersion()}&id=${id()}`,
-})
+autoUpdater.setFeedURL({ url: updateURL })
 
 autoUpdater.on('error', e => {
   logger.error(`update check failed - ${e.message}`)
