9c906fbcb60d669e03482f3b0a31fa5bc0e2d578
Lwrless <happy.lwr@gmail.com>
2023-10-18 01:49:38 +0000

    fix array grammar conversion and add test, format code

--- format/grammar.go
+++ format/grammar.go
@@ -12,199 +12,199 @@ import (
 var spaceRule = "\" \"?"
 
 var primitiveRules = map[string]string{
-	"boolean": `("true" | "false") space`,
-	"number":  `("-"? ([0-9] | [1-9] [0-9]*)) ("." [0-9]+)? ([eE] [-+]? [0-9]+)? space`,
-	"integer": `("-"? ([0-9] | [1-9] [0-9]*)) space`,
-	"string": ` "\"" (
-		[^"\\] |
-		"\\" (["\\/bfnrt] | "u" [0-9a-fA-F] [0-9a-fA-F] [0-9a-fA-F] [0-9a-fA-F])
-		)* "\"" space`,
-	"null": `"null" space`,
+    "boolean": `("true" | "false") space`,
+    "number":  `("-"? ([0-9] | [1-9] [0-9]*)) ("." [0-9]+)? ([eE] [-+]? [0-9]+)? space`,
+    "integer": `("-"? ([0-9] | [1-9] [0-9]*)) space`,
+    "string": ` "\"" (
+        [^"\\] |
+        "\\" (["\\/bfnrt] | "u" [0-9a-fA-F] [0-9a-fA-F] [0-9a-fA-F] [0-9a-fA-F])
+        )* "\"" space`,
+    "null": `"null" space`,
 }
 
 var invalidRuleCharsRegex = regexp.MustCompile(`[^\dA-Za-z-]+`)
 
 var grammarLiteralExcapes = map[string]string{
-	"\n": "\\n",
-	"\r": "\\r",
-	`"`:  `\"`,
+    "\n": "\\n",
+    "\r": "\\r",
+    `"`:  `\"`,
 }
 
 type schemaConverter struct {
-	propOrder []string
-	rules     map[string]string
+    propOrder []string
+    rules     map[string]string
 }
 
 func newSchemaConverter(propOrder []string) *schemaConverter {
-	return &schemaConverter{
-		propOrder: propOrder,
-		rules: map[string]string{
-			"space": spaceRule,
-		},
-	}
+    return &schemaConverter{
+        propOrder: propOrder,
+        rules: map[string]string{
+            "space": spaceRule,
+        },
+    }
 }
 
 func (sc *schemaConverter) formatLiteral(literal any) string {
-	literalBytes, err := json.Marshal(literal)
-	if err != nil {
-		panic(err)
-	}
-	escapedBytes := make([]byte, 0, len(literalBytes)+2)
-	escapedBytes = append(escapedBytes, '"')
-	for _, b := range literalBytes {
-		if escape, ok := grammarLiteralExcapes[string(b)]; ok {
-			escapedBytes = append(escapedBytes, []byte(escape)...)
-		} else {
-			escapedBytes = append(escapedBytes, b)
-		}
-	}
-	escapedBytes = append(escapedBytes, '"')
-	return string(escapedBytes)
+    literalBytes, err := json.Marshal(literal)
+    if err != nil {
+        panic(err)
+    }
+    escapedBytes := make([]byte, 0, len(literalBytes)+2)
+    escapedBytes = append(escapedBytes, '"')
+    for _, b := range literalBytes {
+        if escape, ok := grammarLiteralExcapes[string(b)]; ok {
+            escapedBytes = append(escapedBytes, []byte(escape)...)
+        } else {
+            escapedBytes = append(escapedBytes, b)
+        }
+    }
+    escapedBytes = append(escapedBytes, '"')
+    return string(escapedBytes)
 }
 
 func (sc *schemaConverter) addRule(name string, rule string) string {
-	key := invalidRuleCharsRegex.ReplaceAllString(name, "-")
+    key := invalidRuleCharsRegex.ReplaceAllString(name, "-")
 
-	if existingRule, ok := sc.rules[key]; ok {
-		if existingRule == rule {
-			return key
-		}
+    if existingRule, ok := sc.rules[key]; ok {
+        if existingRule == rule {
+            return key
+        }
 
-		for i := 0; ; i++ {
-			if _, ok := sc.rules[key+fmt.Sprintf("%d", i)]; !ok {
-				key = key + fmt.Sprintf("%d", i)
-				break
-			}
-		}
-	}
+        for i := 0; ; i++ {
+            if _, ok := sc.rules[key+fmt.Sprintf("%d", i)]; !ok {
+                key = key + fmt.Sprintf("%d", i)
+                break
+            }
+        }
+    }
 
-	sc.rules[key] = rule
-	return key
+    sc.rules[key] = rule
+    return key
 }
 
 func (sc *schemaConverter) visit(schema map[string]interface{}, name string) string {
-	schemaType := schema["type"].(string)
-	ruleName := name
-	if ruleName == "" {
-		ruleName = "root"
-	}
+    schemaType := schema["type"].(string)
+    ruleName := name
+    if ruleName == "" {
+        ruleName = "root"
+    }
 
-	switch {
-	case schema["oneOf"] != nil:
-		return sc.compileOneOfAnyOf(schema["oneOf"].([]interface{}), ruleName, name)
-	case schema["anyOf"] != nil:
-		return sc.compileOneOfAnyOf(schema["anyOf"].([]interface{}), ruleName, name)
-	case schema["const"] != nil:
-		return sc.addRule(ruleName, sc.formatLiteral(schema["const"]))
-	case schema["enum"] != nil:
-		return sc.compileEnum(schema["enum"].([]interface{}), ruleName)
-	case schemaType == "object" && schema["properties"] != nil:
-		return sc.compileObject(schema["properties"].(map[string]interface{}), ruleName, name)
-	case schemaType == "array" && schema["items"] != nil:
-		return sc.compileArray(schema["items"].(map[string]interface{}), ruleName, name)
-	default:
-		if primitiveRules[schemaType] == "" {
-			panic(fmt.Sprintf("Unknown schema type: %s", schemaType))
-		}
-		if ruleName != "root" {
-			ruleName = schemaType
-		}
-		return sc.addRule(ruleName, primitiveRules[schemaType])
-	}
+    switch {
+    case schema["oneOf"] != nil:
+        return sc.compileOneOfAnyOf(schema["oneOf"].([]interface{}), ruleName, name)
+    case schema["anyOf"] != nil:
+        return sc.compileOneOfAnyOf(schema["anyOf"].([]interface{}), ruleName, name)
+    case schema["const"] != nil:
+        return sc.addRule(ruleName, sc.formatLiteral(schema["const"]))
+    case schema["enum"] != nil:
+        return sc.compileEnum(schema["enum"].([]interface{}), ruleName)
+    case schemaType == "object" && schema["properties"] != nil:
+        return sc.compileObject(schema["properties"].(map[string]interface{}), ruleName, name)
+    case schemaType == "array" && schema["items"] != nil:
+        return sc.compileArray(schema["items"].(map[string]interface{}), ruleName, name)
+    default:
+        if primitiveRules[schemaType] == "" {
+            panic(fmt.Sprintf("Unknown schema type: %s", schemaType))
+        }
+        if ruleName != "root" {
+            ruleName = schemaType
+        }
+        return sc.addRule(ruleName, primitiveRules[schemaType])
+    }
 }
 
 func (sc *schemaConverter) compileOneOfAnyOf(schemas []interface{}, ruleName, name string) string {
-	var rules = make([]string, len(schemas))
-	for i, altSchema := range schemas {
-		var key string
-		if name == "" {
-			key = fmt.Sprintf("%d", i)
-		} else {
-			key = fmt.Sprintf("%s-%d", name, i)
-		}
-		rules[i] = sc.visit(altSchema.(map[string]interface{}), key)
-	}
-	return sc.addRule(ruleName, strings.Join(rules, " | "))
+    var rules = make([]string, len(schemas))
+    for i, altSchema := range schemas {
+        var key string
+        if name == "" {
+            key = fmt.Sprintf("%d", i)
+        } else {
+            key = fmt.Sprintf("%s-%d", name, i)
+        }
+        rules[i] = sc.visit(altSchema.(map[string]interface{}), key)
+    }
+    return sc.addRule(ruleName, strings.Join(rules, " | "))
 }
 
 func (sc *schemaConverter) compileEnum(literals []interface{}, ruleName string) string {
-	var rules = make([]string, len(literals))
-	for i, literal := range literals {
-		rules[i] = sc.formatLiteral(literal)
-	}
-	return sc.addRule(ruleName, strings.Join(rules, " | "))
+    var rules = make([]string, len(literals))
+    for i, literal := range literals {
+        rules[i] = sc.formatLiteral(literal)
+    }
+    return sc.addRule(ruleName, strings.Join(rules, " | "))
 }
 
 func (sc *schemaConverter) compileObject(properties map[string]interface{}, ruleName, name string) string {
-	propOrder := sc.propOrder
-	propPairs := make([][2]any, 0, len(properties))
-	for k, v := range properties {
-		propPairs = append(propPairs, [2]any{k, v})
-	}
-	if len(propOrder) > 0 {
-		for i, k := range propOrder {
-			for j, pair := range propPairs {
-				if pair[0] == k {
-					propPairs[i], propPairs[j] = propPairs[j], propPairs[i]
-					break
-				}
-			}
-		}
-	}
+    propOrder := sc.propOrder
+    propPairs := make([][2]any, 0, len(properties))
+    for k, v := range properties {
+        propPairs = append(propPairs, [2]any{k, v})
+    }
+    if len(propOrder) > 0 {
+        for i, k := range propOrder {
+            for j, pair := range propPairs {
+                if pair[0] == k {
+                    propPairs[i], propPairs[j] = propPairs[j], propPairs[i]
+                    break
+                }
+            }
+        }
+    }
 
-	var rule strings.Builder
-	rule.WriteString(`"{" space`)
+    var rule strings.Builder
+    rule.WriteString(`"{" space`)
 
-	i := 0
-	for _, propPair := range propPairs {
-		propName := propPair[0].(string)
-		propSchema := propPair[1].(map[string]interface{})
-		var key string
-		if name == "" {
-			key = propName
-		} else {
-			key = fmt.Sprintf("%s-%s", name, propName)
-		}
-		propRuleName := sc.visit(propSchema, key)
-		if i > 0 {
-			rule.WriteString(` "," space`)
-		}
-		fmt.Fprintf(&rule, ` %s space ":" space %s`, sc.formatLiteral(propName), propRuleName)
-		i++
-	}
+    i := 0
+    for _, propPair := range propPairs {
+        propName := propPair[0].(string)
+        propSchema := propPair[1].(map[string]interface{})
+        var key string
+        if name == "" {
+            key = propName
+        } else {
+            key = fmt.Sprintf("%s-%s", name, propName)
+        }
+        propRuleName := sc.visit(propSchema, key)
+        if i > 0 {
+            rule.WriteString(` "," space`)
+        }
+        fmt.Fprintf(&rule, ` %s space ":" space %s`, sc.formatLiteral(propName), propRuleName)
+        i++
+    }
 
-	rule.WriteString(` "}" space`)
-	return sc.addRule(ruleName, rule.String())
+    rule.WriteString(` "}" space`)
+    return sc.addRule(ruleName, rule.String())
 }
 
 func (sc *schemaConverter) compileArray(items map[string]interface{}, ruleName, name string) string {
-	var key string
-	if name == "" {
-		key = "item"
-	} else {
-		key = fmt.Sprintf("%s-item", name)
-	}
-	itemRuleName := sc.visit(items, key)
-	return sc.addRule(ruleName, fmt.Sprintf(`"[" space%s ("," space%s)* "]" space`, itemRuleName, itemRuleName))
+    var key string
+    if name == "" {
+        key = "item"
+    } else {
+        key = fmt.Sprintf("%s-item", name)
+    }
+    itemRuleName := sc.visit(items, key)
+    return sc.addRule(ruleName, fmt.Sprintf(`"[" space (%s ("," space %s)*)? "]" space`, itemRuleName, itemRuleName))
 }
 
 func (sc *schemaConverter) formatGrammar() string {
-	var b strings.Builder
-	for name, rule := range sc.rules {
-		fmt.Fprintf(&b, "%s ::= %s\n", name, rule)
-	}
-	return b.String()
+    var b strings.Builder
+    for name, rule := range sc.rules {
+        fmt.Fprintf(&b, "%s ::= %s\n", name, rule)
+    }
+    return b.String()
 }
 
 // SchemaToGrammar converts a JSON schema to a GNBF grammar,
 // with the given property order (optional).
 func SchemaToGrammar(schema string, propOrder []string) string {
-	sc := newSchemaConverter(propOrder)
-	var schemaObject map[string]interface{}
-	err := json.Unmarshal([]byte(schema), &schemaObject)
-	if err != nil {
-		return ""
-	}
-	sc.visit(schemaObject, "")
-	return sc.formatGrammar()
+    sc := newSchemaConverter(propOrder)
+    var schemaObject map[string]interface{}
+    err := json.Unmarshal([]byte(schema), &schemaObject)
+    if err != nil {
+        return ""
+    }
+    sc.visit(schemaObject, "")
+    return sc.formatGrammar()
 }
--- format/grammar_test.go
+++ format/grammar_test.go
@@ -7,81 +7,123 @@ import (
 	"testing"
 )
 
-const objectSchama = `{
-	"type": "object",
-	"properties": {
-		"foo": {
-			"type": "number"
-		},
-		"bar": {
-			"type": "string"
-		}
-	}
+const objectSchema = `{
+    "type": "object",
+    "properties": {
+        "foo": {
+            "type": "number"
+        },
+        "bar": {
+            "type": "string"
+        }
+    }
 }`
 
 const objectGrammar = `
 space ::= " "?
 number ::= ("-"? ([0-9] | [1-9] [0-9]*)) ("." [0-9]+)? ([eE] [-+]? [0-9]+)? space
 string ::=  "\"" (
-		[^"\\] |
-		"\\" (["\\/bfnrt] | "u" [0-9a-fA-F] [0-9a-fA-F] [0-9a-fA-F] [0-9a-fA-F])
-		)* "\"" space
+        [^"\\] |
+        "\\" (["\\/bfnrt] | "u" [0-9a-fA-F] [0-9a-fA-F] [0-9a-fA-F] [0-9a-fA-F])
+        )* "\"" space
 root ::= "{" space "\"foo\"" space ":" space number "," space "\"bar\"" space ":" space string "}" space
 `
 
-var GRAMMAR_BEGINNING_RE = regexp.MustCompile(`^[\w\d\s]+::=`)
+const arraySchema = `{
+    "type": "array",
+    "items": {
+        "type": "object",
+        "properties": {
+            "firstname": {
+                "type": "string"
+            },
+            "lastname": {
+                "type": "string"
+            },
+            "age": {
+                "type": "integer"
+            },
+            "address": {
+                "type": "string"
+            },
+            "email": {
+                "type": "string"
+            },
+            "isMember": {
+                "type": "boolean"
+            }
+        }
+    }
+}`
+
+const arrayGrammar = `
+space ::= " "?
+string ::=  "\"" (
+        [^"\\] |
+        "\\" (["\\/bfnrt] | "u" [0-9a-fA-F] [0-9a-fA-F] [0-9a-fA-F] [0-9a-fA-F])
+        )* "\"" space
+integer ::= ("-"? ([0-9] | [1-9] [0-9]*)) space
+boolean ::= ("true" | "false") space
+item ::= "{" space "\"address\"" space ":" space string "," space "\"age\"" space ":" space integer "," space "\"email\"" space ":" space string "," space "\"firstname\"" space ":" space string "," space "\"isMember\"" space ":" space boolean "," space "\"lastname\"" space ":" space string "}" space
+root ::= "[" space (item ("," space item)*)? "]" space
+`
+
+var grammarBeginningRegex = regexp.MustCompile(`^[\w\d\s]+::=`)
 
 func readGrammarRules(grammar string) ([]string, error) {
-	scanner := bufio.NewScanner(strings.NewReader(grammar))
-	var rules []string
-	var rule strings.Builder
-	for scanner.Scan() {
-		line := scanner.Text()
-		if GRAMMAR_BEGINNING_RE.MatchString(line) {
-			if rule.Len() > 0 {
-				rules = append(rules, rule.String())
-				rule.Reset()
-			}
-		}
-		rule.WriteString(line)
-	}
-	if rule.Len() > 0 {
-		rules = append(rules, rule.String())
-	}
-	return rules, scanner.Err()
+    scanner := bufio.NewScanner(strings.NewReader(grammar))
+    var rules []string
+    var rule strings.Builder
+    for scanner.Scan() {
+        line := scanner.Text()
+        if grammarBeginningRegex.MatchString(line) {
+            if rule.Len() > 0 {
+                rules = append(rules, rule.String())
+                rule.Reset()
+            }
+        }
+        rule.WriteString(line)
+    }
+    if rule.Len() > 0 {
+        rules = append(rules, rule.String())
+    }
+    return rules, scanner.Err()
 }
 
 func assertGrammaticallyEqual(t *testing.T, actual, expected string) {
-	actualRules, err := readGrammarRules(strings.TrimSpace(actual))
-	if err != nil {
-		t.Errorf("Error reading actual grammar: %s", err)
-		return
-	}
-	expectedRules, err := readGrammarRules(strings.TrimSpace(expected))
-	if err != nil {
-		t.Errorf("Error reading expected grammar: %s", err)
-		return
-	}
+    actualRules, err := readGrammarRules(strings.TrimSpace(actual))
+    if err != nil {
+        t.Errorf("Error reading actual grammar: %s", err)
+        return
+    }
+    expectedRules, err := readGrammarRules(strings.TrimSpace(expected))
+    if err != nil {
+        t.Errorf("Error reading expected grammar: %s", err)
+        return
+    }
 
-	if len(actualRules) != len(expectedRules) {
-		t.Errorf("Expected %d rules, got %d", len(expectedRules), len(actualRules))
-		return
-	}
+    if len(actualRules) != len(expectedRules) {
+        t.Errorf("Expected %d rules, got %d", len(expectedRules), len(actualRules))
+        return
+    }
 
-	expectedSet := make(map[string]bool, len(expectedRules))
-	for _, expectedRule := range expectedRules {
-		expectedSet[expectedRule] = true
-	}
-	for _, actualRule := range actualRules {
-		if !expectedSet[actualRule] {
-			t.Errorf("Unexpected rule found: %s", actualRule)
-			return
-		}
-	}
+    expectedSet := make(map[string]bool, len(expectedRules))
+    for _, expectedRule := range expectedRules {
+        expectedSet[expectedRule] = true
+    }
+    for _, actualRule := range actualRules {
+        if !expectedSet[actualRule] {
+            t.Errorf("Unexpected rule found: %s", actualRule)
+            return
+        }
+    }
 }
 
 func TestGrammar(t *testing.T) {
-	t.Run("object schema to grammar", func(t *testing.T) {
-		assertGrammaticallyEqual(t, SchemaToGrammar(objectSchama, []string{"foo", "bar"}), objectGrammar)
-	})
+    t.Run("object schema to grammar", func(t *testing.T) {
+    	assertGrammaticallyEqual(t, SchemaToGrammar(objectSchema, []string{"foo", "bar"}), objectGrammar)
+    })
+    t.Run("array schema to grammar", func(t *testing.T) {
+        assertGrammaticallyEqual(t, SchemaToGrammar(arraySchema, []string{"address", "age", "email", "firstname", "isMember", "lastname"}), arrayGrammar)
+    })
 }
