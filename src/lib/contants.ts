c5e36c8d11b249dd31d78dd130ffb124edeaf1b1
Timothy J. Baek <timothyjrbeck@gmail.com>
2023-10-18 08:39:38 -0700

    Update contants.ts

--- src/lib/contants.ts
+++ src/lib/contants.ts
@@ -1,7 +1,10 @@
 import { browser, dev } from '$app/environment';
 
-export const ENDPOINT = browser
-	? `http://${location.hostname}:11434`
-	: dev
-	? 'http://127.0.0.1:11434'
-	: 'http://host.docker.internal:11434';
+export const ENDPOINT =
+	process.env.OLLAMA_ENDPOINT != undefined
+		? process.env.OLLAMA_ENDPOINT
+		: browser
+		? `http://${location.hostname}:11434`
+		: dev
+		? 'http://127.0.0.1:11434'
+		: 'http://host.docker.internal:11434';
