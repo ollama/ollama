ecb4d1b8d95a6a982be5b49af89f469ba65f9fcf
Timothy J. Baek <timothyjrbeck@gmail.com>
2023-10-18 21:47:28 -0700

    endpoint error fix

--- src/lib/contants.ts
+++ src/lib/contants.ts
@@ -1,10 +1,7 @@
 import { browser, dev } from '$app/environment';
 
-export const ENDPOINT =
-	process.env.OLLAMA_ENDPOINT != undefined
-		? process.env.OLLAMA_ENDPOINT
-		: browser
-		? `http://${location.hostname}:11434`
-		: dev
-		? 'http://127.0.0.1:11434'
-		: 'http://host.docker.internal:11434';
+export const ENDPOINT = browser
+	? `http://${location.hostname}:11434`
+	: dev
+	? 'http://127.0.0.1:11434'
+	: 'http://host.docker.internal:11434';
