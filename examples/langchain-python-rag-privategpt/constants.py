5c48fe1fb087ef2cc7c1645c06e246349dd66a49
Matt Williams <m@technovangelist.com>
2023-10-30 12:47:56 -0700

    Update examples/langchain-python-rag-privategpt/constants.py
    Co-authored-by: Bruce MacDonald <brucewmacdonald@gmail.com>

--- examples/langchain-python-rag-privategpt/constants.py
+++ examples/langchain-python-rag-privategpt/constants.py
@@ -6,7 +6,6 @@ PERSIST_DIRECTORY = os.environ.get('PERSIST_DIRECTORY', 'db')
 
 # Define the Chroma settings
 CHROMA_SETTINGS = Settings(
-        # chroma_db_impl='duckdb+parquet',
         persist_directory=PERSIST_DIRECTORY,
         anonymized_telemetry=False
 )
