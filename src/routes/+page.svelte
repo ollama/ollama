eac614b4072bcd6f3622c911e45fbd55001df080
Timothy J. Baek <timothyjrbeck@gmail.com>
2023-11-05 15:05:04 -0800

    fix: button dark theme styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -935,7 +935,7 @@
 																</button>
 
 																<button
-																	class=" px-4 py-2.5 hover:bg-gray-100 dark:bg-gray-800 dark:hover:bg-gray-700 text-gray-700 transition outline outline-1 outline-gray-200 dark:outline-gray-600 rounded-lg"
+																	class=" px-4 py-2.5 hover:bg-gray-100 dark:bg-gray-800 dark:hover:bg-gray-700 text-gray-700 dark:text-gray-100 transition outline outline-1 outline-gray-200 dark:outline-gray-600 rounded-lg"
 																	on:click={() => {
 																		cancelEditMessage(messageIdx);
 																	}}
