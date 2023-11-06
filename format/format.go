4152cbd3022215adbce960511fb96cc169b14eaf
Michael Yang <mxyng@pm.me>
2023-11-06 15:17:21 -0800

    explicit rounding

--- format/format.go
+++ format/format.go
@@ -1,6 +1,9 @@
 package format
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 const (
 	Thousand = 1000
@@ -11,11 +14,11 @@ const (
 func Human(b uint64) string {
 	switch {
 	case b > Billion:
-		return fmt.Sprintf("%dB", b/Billion)
+		return fmt.Sprintf("%.0fB", math.Round(float64(b)/Billion))
 	case b > Million:
-		return fmt.Sprintf("%dM", b/Million)
+		return fmt.Sprintf("%.0fM", math.Round(float64(b)/Million))
 	case b > Thousand:
-		return fmt.Sprintf("%dK", b/Thousand)
+		return fmt.Sprintf("%.0fK", math.Round(float64(b)/Thousand))
 	default:
 		return fmt.Sprintf("%d", b)
 	}
