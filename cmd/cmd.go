31ab453d37f08e7f13f0bda37d9963755de23632
Bruce MacDonald <brucewmacdonald@gmail.com>
2023-11-20 16:43:48 -0500

    resolve FROM path before sending modelfile (#1211)

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -82,6 +82,10 @@ func CreateHandler(cmd *cobra.Command, args []string) error {
 				path = filepath.Join(home, path[2:])
 			}
 
+			if !filepath.IsAbs(path) {
+				path = filepath.Join(filepath.Dir(filename), path)
+			}
+
 			bin, err := os.Open(path)
 			if errors.Is(err, os.ErrNotExist) && c.Name == "model" {
 				continue
