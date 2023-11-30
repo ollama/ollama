fb6003de84765e8d21ce136183446fe57bb66ce1
ddang <ddang@pop-os.localdomain>
2023-11-29 19:39:37 -0800

    Comments resolved

--- src/lib/components/layout/Sidebar.svelte
+++ src/lib/components/layout/Sidebar.svelte
@@ -24,7 +24,7 @@
 
 	let showDropdown = false;
 
-  let showWarningHistory = false;
+  let showDeleteHistoryConfirm = false;
 
 	onMount(async () => {
 		if (window.innerWidth > 1280) {
@@ -449,7 +449,7 @@
 						<div class=" self-center">Export</div>
 					</button>
 				</div>
-        {#if showWarningHistory}
+        {#if showDeleteHistoryConfirm}
           <div class="flex justify-between rounded-md items-center py-3 px-3.5 w-full transition">
             <div class="flex items-center">
               <svg 
@@ -466,7 +466,7 @@
                   d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
                 />
               </svg>
-              <span class="text-red-500">Are you sure?</span>
+              <span>Are you sure?</span>
             </div>
             
             <div class="flex space-x-1.5 items-center">
@@ -474,7 +474,7 @@
                 class="hover:text-white transition"
                 on:click={() => {
                   deleteChatHistory();
-                  showWarningHistory = false;
+                  showDeleteHistoryConfirm = false;
                 }}
               >
                 <svg
@@ -493,7 +493,7 @@
               <button
                 class="hover:text-white transition"
                 on:click={() => {
-                  showWarningHistory = false;
+                  showDeleteHistoryConfirm = false;
                 }}
               >
                 <svg
@@ -513,7 +513,7 @@
           <button
 					class=" flex rounded-md py-3 px-3.5 w-full hover:bg-gray-900 transition"
 					on:click={() => {
-            showWarningHistory = true;
+            showDeleteHistoryConfirm = true;
 					}}>
             <div class="mr-3">
               <svg
