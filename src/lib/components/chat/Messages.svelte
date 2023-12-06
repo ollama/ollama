0fb5aee62e86d1aa5823350acfe8958454f11584
Timothy J. Baek <timothyjrbeck@gmail.com>
2023-12-06 08:19:20 -0800

    fix: wip code commented

--- src/lib/components/chat/Messages.svelte
+++ src/lib/components/chat/Messages.svelte
@@ -664,7 +664,7 @@
 													</div>
 												{/if}
 
-												<button
+												<!-- <button
 													class="invisible group-hover:visible p-1 rounded dark:hover:bg-gray-800 transition"
 													on:click={() => {
 														editMessageHandler(message.id);
@@ -684,7 +684,7 @@
 															d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"
 														/>
 													</svg>
-												</button>
+												</button> -->
 
 												<button
 													class="{messageIdx + 1 === messages.length
