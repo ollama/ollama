ef63bdd8aade326bf389297e70d3a684fe4bf51b
ibbobud <149419459+ibbobud@users.noreply.github.com>
2023-12-12 23:22:35 -0500

    Changed formating for the following files:   rhel9-docker/docker-compose.yaml   rhel9-docker/entrypoint.sh

--- rhel9-docker/entrypoint.sh
+++ rhel9-docker/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-
+# Run the ollama service and wait 2 seconds for it to start
 ollama serve & \
 sleep 2 && \
+# Pull the model declared in the MODEL environment variable
 ollama pull $MODEL && \
+# Run litellm with the environment variables
 litellm --model ollama/$MODEL --config ./config.yaml --host $HOST --port $PORT 
