c9d8645abafcca52f95cad9ca7ff7a9d790ef719
Timothy J. Baek <timothyjrbeck@gmail.com>
2023-12-14 01:05:38 -0800

    feat: share button added

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -566,7 +566,7 @@
 	}}
 />
 
-<Navbar {title} />
+<Navbar {title} shareEnabled={messages.length > 0} />
 <div class="min-h-screen w-full flex justify-center">
 	<div class=" py-2.5 flex flex-col justify-between w-full">
 		<div class="max-w-2xl mx-auto w-full px-3 md:px-0 mt-10">
