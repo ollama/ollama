10ba4c529fba458dc9cebce90844208477538be5
Daniel Hiltgen <daniel@ollama.com>
2023-12-24 11:03:15 -0800

    Refactor builder dockerfile
    Reorganize the x86/arm components to be more DRY, and
    remove the cuda driver

--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -5,10 +5,18 @@ set -eu
 export VERSION=${VERSION:-0.0.0}
 export GOFLAGS="'-ldflags=-w -s \"-X=github.com/jmorganca/ollama/version.Version=$VERSION\" \"-X=github.com/jmorganca/ollama/server.mode=release\"'"
 
+if docker buildx inspect | grep "^Driver:" | grep "docker-container" >/dev/null; then
+    echo "Caching Enabled"
+    CACHE_FLAGS="--cache-from type=local,src=.cache --cache-to type=local,dest=.cache"
+else
+    echo "Caching Disabled"
+    CACHE_FLAGS=""
+fi
+
 mkdir -p dist
 
 for TARGETARCH in amd64 arm64; do
-    docker buildx build --load --platform=linux/$TARGETARCH --build-arg=VERSION --build-arg=GOFLAGS --build-arg=CGO_CFLAGS --cache-from type=local,src=.cache --cache-to type=local,dest=.cache -f Dockerfile.build -t builder:$TARGETARCH .
+    docker buildx build --load --platform=linux/$TARGETARCH --build-arg=VERSION --build-arg=GOFLAGS --build-arg=CGO_CFLAGS ${CACHE_FLAGS} -f Dockerfile.build -t builder:$TARGETARCH .
     docker create --platform linux/$TARGETARCH --name builder-$TARGETARCH builder:$TARGETARCH
     docker cp builder-$TARGETARCH:/go/src/github.com/jmorganca/ollama/ollama ./dist/ollama-linux-$TARGETARCH
     docker rm builder-$TARGETARCH
