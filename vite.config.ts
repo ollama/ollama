5ee58e1c7963a1772f2cc0d0dfba8a07b8a28963
Anuraag Jain <thehray@gmail.com>
2023-12-28 20:19:09 +0200

    revert: vite config, hmr works without it

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	server: {
-		hmr: true
-	}
+	plugins: [sveltekit()]
 });
