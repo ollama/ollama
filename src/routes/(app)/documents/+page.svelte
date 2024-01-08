edceeba1b0cb53ce12d80d8f1298f9ad53d29606
Timothy J. Baek <timothyjrbeck@gmail.com>
2024-01-08 02:04:20 -0800

    feat: help message added

--- src/routes/(app)/documents/+page.svelte
+++ src/routes/(app)/documents/+page.svelte
@@ -418,6 +418,29 @@
 					</div>
 				</div>
 			{/if}
+
+			<div class="text-xs flex items-center space-x-1">
+				<div>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						class="w-3 h-3"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z"
+						/>
+					</svg>
+				</div>
+
+				<div class="line-clamp-1">
+					Tip: Use '#' in the prompt input to swiftly load and select your documents.
+				</div>
+			</div>
 		</div>
 	</div>
 </div>
