93c73d355bd7bd83fb9d6b774b3b073e911f9e80
Tanvir <tanvir.ahmed.tonoy@skiff.com>
2024-01-09 13:28:35 +0600

    chore(docker): update ollama docker image tag to latest

--- run-ollama-docker.sh
+++ run-ollama-docker.sh
@@ -3,7 +3,7 @@
 read -r -p "Do you want ollama in docker with GPU support? (y/n): " use_gpu
 
 docker rm -f ollama || true
-docker pull ollama/ollama
+docker pull ollama/ollama:latest
 
 if [ "$use_gpu" == "y" ]; then
     docker run -d --gpus=all -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama
