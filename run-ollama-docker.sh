0c0c5955c61ae64422374a53bd72a6e36f714dad
Tanvir <tanvir.ahmed.tonoy@skiff.com>
2024-01-09 13:27:39 +0600

    feat(docker): add option to run ollama w/o GPU support

--- run-ollama-docker.sh
+++ run-ollama-docker.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+read -r -p "Do you want ollama in docker with GPU support? (y/n): " use_gpu
+
 docker rm -f ollama || true
 docker pull ollama/ollama
-# CPU Only
-docker run -d -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama
-# GPU Support
-# docker run -d --gpus=all -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama
+
+if [ "$use_gpu" == "y" ]; then
+    docker run -d --gpus=all -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama
+else
+    docker run -d -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama
+fi
+
 docker image prune -f
