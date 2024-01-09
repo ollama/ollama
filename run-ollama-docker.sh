f87a1c95435832ad21f5e32ac57bf1bd5ae4c80c
Tanvir <tanvir.ahmed.tonoy@skiff.com>
2024-01-09 13:30:58 +0600

    refactor: update run-ollama-docker.sh

--- run-ollama-docker.sh
+++ run-ollama-docker.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-read -r -p "Do you want ollama in docker with GPU support? (y/n): " use_gpu
+read -r -p "Do you want ollama in Docker with GPU support? (y/n): " use_gpu
 
 docker rm -f ollama || true
 docker pull ollama/ollama:latest
 
+docker_args="-d -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama"
+
 if [ "$use_gpu" == "y" ]; then
-    docker run -d --gpus=all -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama
-else
-    docker run -d -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama
+    docker_args+=" --gpus=all"
 fi
+
+docker run "$docker_args"
 
 docker image prune -f
