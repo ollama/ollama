fd33f68f284512e0f55ba07dabe0083c3132e46a
Tanvir <tanvir.ahmed.tonoy@skiff.com>
2024-01-09 13:43:55 +0600

    refactor(run.sh): update with port variables

--- run.sh
+++ run.sh
@@ -2,12 +2,14 @@
 
 image_name="ollama-webui"
 container_name="ollama-webui"
+host_port=3000
+container_port=8080
 
 docker build -t "$image_name" .
 docker stop "$container_name" &>/dev/null || true
 docker rm "$container_name" &>/dev/null || true
 
-docker run -d -p 3000:8080 \
+docker run -d -p "$host_port":"$container_port" \
     --add-host=host.docker.internal:host-gateway \
     -v "${image_name}:/app/backend/data" \
     --name "$container_name" \
