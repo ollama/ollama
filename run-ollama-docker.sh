db51c22093f8bdee48a5444dbbe4fb30cd2b1b98
Tanvir <tanvir.ahmed.tonoy@skiff.com>
2024-01-09 13:46:07 +0600

    refactor(run-ollama-docker.sh): update with port variables

--- run-ollama-docker.sh
+++ run-ollama-docker.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+host_port=11434
+container_port=11434
+
 read -r -p "Do you want ollama in Docker with GPU support? (y/n): " use_gpu
 
 docker rm -f ollama || true
 docker pull ollama/ollama:latest
 
-docker_args="-d -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama"
+docker_args="-d -v ollama:/root/.ollama -p $host_port:$container_port --name ollama ollama/ollama"
 
 if [ "$use_gpu" == "y" ]; then
     docker_args+=" --gpus=all"
