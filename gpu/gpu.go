552db98bf111ade526b351d7d1a0f8f1f950d359
Daniel Hiltgen <daniel@ollama.com>
2024-01-19 13:23:29 -0800

    More WSL paths

--- gpu/gpu.go
+++ gpu/gpu.go
@@ -38,6 +38,7 @@ var CudaLinuxGlobs = []string{
 	"/usr/lib/x86_64-linux-gnu/nvidia/current/libnvidia-ml.so*",
 	"/usr/lib/x86_64-linux-gnu/libnvidia-ml.so*",
 	"/usr/lib/wsl/lib/libnvidia-ml.so*",
+	"/usr/lib/wsl/drivers/*/libnvidia-ml.so*",
 	"/opt/cuda/lib64/libnvidia-ml.so*",
 	"/opt/cuda/targets/x86_64-linux/lib/stubs/libnvidia-ml.so*",
 	"/usr/lib*/libnvidia-ml.so*",
