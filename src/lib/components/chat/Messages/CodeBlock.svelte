52e7684ff4da566b77712b7a97834c06e1b4f3f2
Timothy J. Baek <timothyjrbeck@gmail.com>
2024-01-22 04:14:07 -0800

    fix: code block unmount issue

--- src/lib/components/chat/Messages/CodeBlock.svelte
+++ src/lib/components/chat/Messages/CodeBlock.svelte
@@ -20,17 +20,19 @@
 	$: highlightedCode = code ? hljs.highlightAuto(code, hljs.getLanguage(lang)?.aliases).value : '';
 </script>
 
-<div class="mb-4">
-	<div
-		class="flex justify-between bg-[#202123] text-white text-xs px-4 pt-1 pb-0.5 rounded-t-lg overflow-x-auto"
-	>
-		<div class="p-1">{lang}</div>
-		<button class="copy-code-button bg-none border-none p-1" on:click={copyCode}
-			>{copied ? 'Copied' : 'Copy Code'}</button
+{#if code}
+	<div class="mb-4">
+		<div
+			class="flex justify-between bg-[#202123] text-white text-xs px-4 pt-1 pb-0.5 rounded-t-lg overflow-x-auto"
 		>
-	</div>
+			<div class="p-1">{@html lang}</div>
+			<button class="copy-code-button bg-none border-none p-1" on:click={copyCode}
+				>{copied ? 'Copied' : 'Copy Code'}</button
+			>
+		</div>
 
-	<pre class=" rounded-b-lg hljs p-4 px-5 overflow-x-auto rounded-t-none"><code
-			class="language-{lang} rounded-t-none whitespace-pre">{@html highlightedCode || code}</code
-		></pre>
-</div>
+		<pre class=" rounded-b-lg hljs p-4 px-5 overflow-x-auto rounded-t-none"><code
+				class="language-{lang} rounded-t-none whitespace-pre">{@html highlightedCode || code}</code
+			></pre>
+	</div>
+{/if}
