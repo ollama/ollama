03a7e35967dde9f75f2cc3e752ecb74c887feae5
Tim Farrell <tim@thecookiejar.me>
2024-02-01 13:43:54 -0600

    Default docker installations should generate a random key instead of using a static secret that everyone can see.

--- backend/start.sh
+++ backend/start.sh
@@ -4,4 +4,9 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$SCRIPT_DIR" || exit
 
 PORT="${PORT:-8080}"
-exec uvicorn main:app --host 0.0.0.0 --port "$PORT" --forwarded-allow-ips '*'
+if test -f docker_secret_key && test "$WEBUI_SECRET_KEY" = ""; then
+  echo Using generated DOCKER_SECRET_KEY
+  WEBUI_SECRET_KEY=`cat docker_secret_key`
+fi
+
+WEBUI_SECRET_KEY="$WEBUI_SECRET_KEY" exec uvicorn main:app --host 0.0.0.0 --port "$PORT" --forwarded-allow-ips '*'
