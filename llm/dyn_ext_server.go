2f804068bd4db03c94e3461d90ee0bb9dc823a8e
Bruce MacDonald <brucewmacdonald@gmail.com>
2024-03-12 19:07:11 -0400

    warn when json format is expected but not mentioned in prompt (#3081)

--- llm/dyn_ext_server.go
+++ llm/dyn_ext_server.go
@@ -198,6 +198,9 @@ func (llm *dynExtServer) Predict(ctx context.Context, predict PredictOpts, fn fu
 
 	if predict.Format == "json" {
 		request["grammar"] = jsonGrammar
+		if !strings.Contains(strings.ToLower(predict.Prompt), "json") {
+			slog.Warn("Prompt does not specify that the LLM should response in JSON, but JSON format is expected. For best results specify that JSON is expected in the system prompt.")
+		}
 	}
 
 	retryDelay := 100 * time.Microsecond
