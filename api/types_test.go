d8b6b4b13459f954dbd7045cdf984491a84519a4
Patrick Devine <patrick@infrahq.com>
2024-03-13 13:12:06 -0700

    remove struct duration test

--- api/types_test.go
+++ api/types_test.go
@@ -10,41 +10,6 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestKeepAliveParsingFromStruct(t *testing.T) {
-	tests := []struct {
-		name string
-		req  *ChatRequest
-		exp  *Duration
-	}{
-		{
-			name: "Positive Duration",
-			req: &ChatRequest{
-				KeepAlive: &Duration{Duration: 42 * time.Minute},
-			},
-			exp: &Duration{42 * time.Minute},
-		},
-		{
-			name: "Negative Duration",
-			req: &ChatRequest{
-				KeepAlive: &Duration{Duration: -1 * time.Minute},
-			},
-			exp: &Duration{math.MaxInt64},
-		},
-	}
-
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			ser, err := json.Marshal(test.req)
-			require.NoError(t, err)
-
-			var dec ChatRequest
-			err = json.Unmarshal([]byte(ser), &dec)
-			require.NoError(t, err)
-
-			assert.Equal(t, test.exp, dec.KeepAlive)
-		})
-	}
-}
 func TestKeepAliveParsingFromJSON(t *testing.T) {
 	tests := []struct {
 		name string
