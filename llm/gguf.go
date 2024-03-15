988593c9cf35f7a4acd783cfd3faa7d17a57a18c
Blake Mizerany <blake.mizerany@gmail.com>
2024-03-15 07:11:12 -0700

    Update llm/gguf.go
    Co-authored-by: Jeffrey Morgan <jmorganca@gmail.com>

--- llm/gguf.go
+++ llm/gguf.go
@@ -541,7 +541,7 @@ func (llm *GGUFModel) Encode(f *os.File) error {
 			b, err := io.ReadFull(dataFile, data)
 			remaining -= uint64(b)
 
-			if errors.Is(io.EOF) || remaining <= 0 {
+			if errors.Is(err, io.EOF) || remaining <= 0 {
 				finished = true
 			} else if err != nil {
 				return err
