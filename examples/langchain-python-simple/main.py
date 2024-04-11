d3978e417bccb01ba3d7a25d7debb1f4c62d039e
Simon Schampijer <3714785+erikos@users.noreply.github.com>
2024-04-11 10:45:38 +0200

    Better formatting of input prompt
    Move the question to a new line and
    lead with an prompt indicator character.

--- examples/langchain-python-simple/main.py
+++ examples/langchain-python-simple/main.py
@@ -1,6 +1,6 @@
 from langchain.llms import Ollama
 
-input = input("What is your question?")
+input = input("What is your question? \n> ")
 llm = Ollama(model="llama2")
 res = llm.predict(input)
 print (res)
