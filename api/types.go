97d82fe12f2e1e817a4e0c93c1b0c3f3ca2c2bd8
jmorganca <jmorganca@gmail.com>
2024-04-25 18:42:28 -0400

    add comment

--- api/types.go
+++ api/types.go
@@ -396,7 +396,9 @@ func (opts *Options) FromMap(m map[string]interface{}) error {
 func DefaultOptions() Options {
 	return Options{
 		// options set on request to runner
-		NumPredict:       -1,
+		NumPredict: -1,
+
+		// set a minimal num_keep to avoid issues on context shifts
 		NumKeep:          4,
 		Temperature:      0.8,
 		TopK:             40,
