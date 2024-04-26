5db1a356a296e482efff8bc480ee4a2e823cbd6c
John Zila <john@jzila.com>
2024-04-26 16:55:31 -0500

    util: load the size of env vars dynamically when parsing an int

--- server/util.go
+++ server/util.go
@@ -3,6 +3,7 @@ package server
 import (
 	"os"
 	"strconv"
+	"unsafe"
 )
 
 type Int interface {
@@ -10,7 +11,7 @@ type Int interface {
 }
 
 func parseInt[T Int](v string, fallback T) T {
-	i, err := strconv.ParseInt(v, 10, 64)
+	i, err := strconv.ParseInt(v, 10, int(unsafe.Sizeof(fallback)))
 	if err != nil {
 		return fallback
 	}
