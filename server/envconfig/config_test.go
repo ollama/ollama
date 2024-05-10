824ee5446f498092653e647fdef2167d28e33b4b
Daniel Hiltgen <daniel@ollama.com>
2024-05-10 16:49:48 -0700

    Fix envconfig unit test

--- server/envconfig/config_test.go
+++ server/envconfig/config_test.go
@@ -1,20 +1,20 @@
 package envconfig
 
 import (
-	"os"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
 func TestConfig(t *testing.T) {
-	os.Setenv("OLLAMA_DEBUG", "")
+	Debug = false // Reset whatever was loaded in init()
+	t.Setenv("OLLAMA_DEBUG", "")
 	LoadConfig()
 	require.False(t, Debug)
-	os.Setenv("OLLAMA_DEBUG", "false")
+	t.Setenv("OLLAMA_DEBUG", "false")
 	LoadConfig()
 	require.False(t, Debug)
-	os.Setenv("OLLAMA_DEBUG", "1")
+	t.Setenv("OLLAMA_DEBUG", "1")
 	LoadConfig()
 	require.True(t, Debug)
 }
