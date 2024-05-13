f8464785a66461496fa1291cd68630d8540171d5
Josh Yan <joshyan@Joshs-MBP.localdomain>
2024-05-13 14:50:52 -0700

    removed inconsistencies

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1091,8 +1091,8 @@ Environment Variables:
 
     OLLAMA_HOST         The host:port to bind to (default "127.0.0.1:11434")
     OLLAMA_ORIGINS      A comma separated list of allowed origins
-    OLLAMA_MODELS       The path to the models directory (default is "~/.ollama/models")
-    OLLAMA_KEEP_ALIVE   The duration that models stay loaded in memory (default is "5m")
+    OLLAMA_MODELS       The path to the models directory (default "~/.ollama/models")
+    OLLAMA_KEEP_ALIVE   The duration that models stay loaded in memory (default "5m")
     OLLAMA_DEBUG        Set to 1 to enable additional debug logging
 `)
 
