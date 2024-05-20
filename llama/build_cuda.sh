922d0acbdb22d7efb11572305bedb1bffccf84d6
jmorganca <jmorganca@gmail.com>
2024-05-20 16:17:13 -0400

    improve cuda and hipblas build scripts

--- llama/build_cuda.sh
+++ llama/build_cuda.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+
+os="$(uname -s)"
+
+if [[ "$os" == "Windows_NT" || "$os" == "MINGW64_NT"* ]]; then
+    output="ggml-cuda.dll"
+else
+    output="libggml-cuda.so"
+fi
+
 nvcc \
     -t 12 \
     --generate-code=arch=compute_50,code=[compute_50,sm_50] \
@@ -14,6 +24,7 @@ nvcc \
     -DGGML_BUILD=1 \
     -DGGML_USE_LLAMAFILE \
     -D_GNU_SOURCE \
+    -DCMAKE_POSITION_INDEPENDENT_CODE=on \
     -Wno-deprecated-gpu-targets \
     --forward-unknown-to-host-compiler \
     -use_fast_math \
@@ -23,5 +34,5 @@ nvcc \
     -I. \
     -lcuda -lcublas -lcudart -lcublasLt \
     -O3 \
-    -o libggml-cuda.so \
+    -o $output \
     ggml-cuda.cu ggml-cuda/*.cu ggml.c ggml-backend.c ggml-alloc.c ggml-quants.c sgemm.cpp
