8945f0d78b5591f1fc7fafad767d060bb569f9f3
jmorganca <jmorganca@gmail.com>
2024-05-19 23:11:30 -0700

    cuda linux

--- llama/build_cuda.sh
+++ llama/build_cuda.sh
@@ -8,19 +8,20 @@ nvcc \
     --generate-code=arch=compute_80,code=[compute_80,sm_80] \
     -DGGML_CUDA_DMMV_X=32 \
     -DGGML_CUDA_PEER_MAX_BATCH_SIZE=128 \
-    -DGGML_MULTIPLATFORM \
     -DGGML_CUDA_MMV_Y=1 \
     -DGGML_USE_CUDA=1 \
     -DGGML_SHARED=1 \
     -DGGML_BUILD=1 \
     -DGGML_USE_LLAMAFILE \
+    -D_GNU_SOURCE \
     -Wno-deprecated-gpu-targets \
     --forward-unknown-to-host-compiler \
     -use_fast_math \
     -link \
     -shared \
+    -fPIC \
     -I. \
     -lcuda -lcublas -lcudart -lcublasLt \
     -O3 \
-    -o ggml-cuda.dll \
+    -o libggml-cuda.so \
     ggml-cuda.cu ggml-cuda/*.cu ggml.c ggml-backend.c ggml-alloc.c ggml-quants.c sgemm.cpp
