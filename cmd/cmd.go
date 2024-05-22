201d853fdf4d08bdbf0c7e58087ba5d574feace1
Michael Yang <mxyng@pm.me>
2024-05-22 08:52:00 -0700

    nolintlint

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1029,22 +1029,6 @@ func initializeKeypair() error {
 	return nil
 }
 
-func waitForServer(ctx context.Context, client *api.Client) error {
-	// wait for the server to start
-	timeout := time.After(5 * time.Second)
-	tick := time.Tick(500 * time.Millisecond)
-	for {
-		select {
-		case <-timeout:
-			return errors.New("timed out waiting for server to start")
-		case <-tick:
-			if err := client.Heartbeat(ctx); err == nil {
-				return nil // server has started
-			}
-		}
-	}
-}
-
 func checkServerHeartbeat(cmd *cobra.Command, _ []string) error {
 	client, err := api.ClientFromEnvironment()
 	if err != nil {
