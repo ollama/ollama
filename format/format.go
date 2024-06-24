f93cdfdfae47fb859f5f395635331bee0a8eb142
Roy Han <royjhan02@gmail.com>
2024-06-24 10:53:15 -0700

    Standardize with ollama.com

--- format/format.go
+++ format/format.go
@@ -2,32 +2,41 @@ package format
 
 import (
 	"fmt"
-	"math"
-)
-
-const (
-	Thousand = 1000
-	Million  = Thousand * 1000
-	Billion  = Million * 1000
 )
 
 func HumanNumber(b uint64) string {
+	const (
+		Thousand = 1000
+		Million  = Thousand * 1000
+		Billion  = Million * 1000
+		Trillion = Billion * 1000
+	)
+
 	switch {
+	case b >= Trillion:
+		number := float64(b) / Trillion
+		return fmt.Sprintf("%sT", decimalPlace(number))
 	case b >= Billion:
 		number := float64(b) / Billion
-		if number == math.Floor(number) {
-			return fmt.Sprintf("%.0fB", number) // no decimals if whole number
-		}
-		return fmt.Sprintf("%.2fB", number) // two decimals if not a whole number
+		return fmt.Sprintf("%sB", decimalPlace(number))
 	case b >= Million:
 		number := float64(b) / Million
-		if number == math.Floor(number) {
-			return fmt.Sprintf("%.0fM", number) // no decimals if whole number
-		}
-		return fmt.Sprintf("%.2fM", number) // two decimals if not a whole number
+		return fmt.Sprintf("%sM", decimalPlace(number))
 	case b >= Thousand:
-		return fmt.Sprintf("%.0fK", float64(b)/Thousand)
+		number := float64(b) / Thousand
+		return fmt.Sprintf("%sK", decimalPlace(number))
 	default:
 		return fmt.Sprintf("%d", b)
+	}
+}
+
+func decimalPlace(number float64) string {
+	switch {
+	case number >= 100:
+		return fmt.Sprintf("%.0f", number)
+	case number >= 10:
+		return fmt.Sprintf("%.1f", number)
+	default:
+		return fmt.Sprintf("%.2f", number)
 	}
 }
