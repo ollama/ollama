5d76e78c2ff0bb8769885a91105aef077fb2b4cc
Josh Yan <jyan00017@gmail.com>
2024-07-01 15:43:03 -0700

    add error message for unsupported arch

--- llm/status.go
+++ llm/status.go
@@ -34,6 +34,19 @@ func (w *StatusWriter) Write(b []byte) (int, error) {
 			errMsg = prefix + string(bytes.TrimSpace(after))
 		}
 	}
+
+	if bytes.Contains(b, []byte("unknown model architecture")) {
+		if _, after, ok := bytes.Cut(b, []byte("architecture")); ok {
+			errMsg = "error" + string(bytes.TrimSpace(after))
+
+			if before, _, ok := bytes.Cut(after, []byte("llama_load")); ok {
+				errMsg = "error" + string(bytes.TrimSpace(before))
+			}
+
+			errMsg = errMsg + "\nYour current version of Ollama doesn't support this model architecture. Consider upgrading."
+		}
+	}
+
 	if errMsg != "" {
 		w.LastErrMsg = errMsg
 	}
