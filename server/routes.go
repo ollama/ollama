1a6197abb1e774875f7e6b77786baa57b4a64834
Josh Yan <jyan00017@gmail.com>
2024-07-03 17:05:22 -0700

    rm config

--- server/routes.go
+++ server/routes.go
@@ -1075,7 +1075,6 @@ func (s *Server) GenerateRoutes() http.Handler {
 	r.POST("/api/blobs/:digest", s.CreateBlobHandler)
 	r.HEAD("/api/blobs/:digest", s.HeadBlobHandler)
 	r.GET("/api/ps", s.ProcessHandler)
-	r.GET("/api/config", s.ConfigHandler)
 
 	// Compatibility endpoints
 	r.POST("/v1/chat/completions", openai.ChatMiddleware(), s.ChatHandler)
@@ -1428,8 +1427,4 @@ func handleScheduleError(c *gin.Context, name string, err error) {
 	default:
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 	}
-}
-
-func (s *Server) ConfigHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, api.ServerConfig{ModelDir: envconfig.ModelsDir})
 }
