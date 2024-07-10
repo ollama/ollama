14d22902a74ea18302affe313083792e636ae844
Yuri Khrustalev <yuri.khrustalev@gmail.com>
2024-07-10 13:15:00 +0000

    Configure the systemd service via a separate file.
    Instead of changing the systemd unit file, use a separate config for
    the configuration.
    
    refs #3516

--- scripts/install.sh
+++ scripts/install.sh
@@ -99,6 +99,18 @@ configure_systemd() {
     status "Adding current user to ollama group..."
     $SUDO usermod -a -G ollama $(whoami)
 
+    if $SUDO test -f "/etc/ollama/serve.conf" ; then
+        status "Skip creating ollama serve config file..."
+    else
+        status "Creating ollama serve config file..."
+        $SUDO mkdir -p /etc/ollama/
+        cat <<EOF | $SUDO tee /etc/ollama/serve.conf >/dev/null
+# The list of supported env variables https://github.com/ollama/ollama/blob/main/envconfig/config.go
+#OLLAMA_DEBUG=1
+#OLLAMA_HOST=0.0.0.0:11434
+EOF
+    fi
+
     status "Creating ollama systemd service..."
     cat <<EOF | $SUDO tee /etc/systemd/system/ollama.service >/dev/null
 [Unit]
@@ -112,6 +124,7 @@ Group=ollama
 Restart=always
 RestartSec=3
 Environment="PATH=$PATH"
+EnvironmentFile=/etc/ollama/serve.conf
 
 [Install]
 WantedBy=default.target
