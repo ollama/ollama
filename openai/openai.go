b95e783317e77cd0b417c9dbd60969a6a76bb8f1
Roy Han <royjhan02@gmail.com>
2024-07-10 14:25:25 -0700

    remove TODO

--- openai/openai.go
+++ openai/openai.go
@@ -189,7 +189,6 @@ func toChatCompletion(id string, r api.ChatResponse) ChatCompletion {
 			}(r.DoneReason),
 		}},
 		Usage: Usage{
-			// TODO: ollama returns 0 for prompt eval if the prompt was cached, but openai returns the actual count
 			PromptTokens:     r.PromptEvalCount,
 			CompletionTokens: r.EvalCount,
 			TotalTokens:      r.PromptEvalCount + r.EvalCount,
