e210f8763f127f67a8a23cfd2a7759251f67d128
Roy Han <royjhan02@gmail.com>
2024-07-12 15:09:05 -0700

    merge conflicts

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -672,9 +672,6 @@ func showInfo(resp *api.ShowResponse) {
 		projectorData := [][]string{
 			{"arch", "clip"},
 			{"parameters", format.Parameters(uint64(resp.ProjectorInfo["general.parameter_count"].(float64)))},
-			{"projector type", resp.ProjectorInfo["clip.projector_type"].(string)},
-			{"embedding length", fmt.Sprintf("%v", resp.ProjectorInfo["clip.vision.embedding_length"].(float64))},
-			{"projection dimensionality", fmt.Sprintf("%v", resp.ProjectorInfo["clip.vision.projection_dim"].(float64))},
 		}
 
 		if projectorType, ok := resp.ProjectorInfo["clip.projector_type"]; ok {
