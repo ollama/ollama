10e768826c7d5a8f7d7fab13832299a466a01f87
Josh <76125168+joshyan1@users.noreply.github.com>
2024-07-11 17:24:29 -0700

    fix: quant err message (#5616)

--- llm/llm.go
+++ llm/llm.go
@@ -33,7 +33,7 @@ func Quantize(infile, outfile string, ftype fileType) error {
 	params.ftype = ftype.Value()
 
 	if rc := C.llama_model_quantize(cinfile, coutfile, &params); rc != 0 {
-		return fmt.Errorf("llama_model_quantize: %d", rc)
+		return fmt.Errorf("failed to quantize model. This model architecture may not be supported, or you may need to upgrade Ollama to the latest version")
 	}
 
 	return nil
