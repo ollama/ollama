7e313e59645ce68fef125c935f3a130c046c306f
Roy Han <royjhan02@gmail.com>
2024-07-12 16:37:29 -0700

    remove redundant error check

--- server/routes.go
+++ server/routes.go
@@ -326,10 +326,6 @@ func (s *Server) EmbedHandler(c *gin.Context) {
 			}
 		}
 
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-			return
-		}
 		reqEmbedArray[i] = s
 	}
 	embeddings, err := r.Embed(c.Request.Context(), reqEmbedArray)
