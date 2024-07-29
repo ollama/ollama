9c2449122e9844587de382e05fa45ece1738da81
Roy Han <royjhan02@gmail.com>
2024-07-29 11:16:06 -0700

    backend stream support

--- server/routes.go
+++ server/routes.go
@@ -1299,6 +1299,8 @@ func (s *Server) ChatHandler(c *gin.Context) {
 	caps := []Capability{CapabilityCompletion}
 	if len(req.Tools) > 0 {
 		caps = append(caps, CapabilityTools)
+		stream := false
+		req.Stream = &stream
 	}
 
 	r, m, opts, err := s.scheduleRunner(c.Request.Context(), req.Model, caps, req.Options, req.KeepAlive)
@@ -1397,6 +1399,7 @@ func (s *Server) ChatHandler(c *gin.Context) {
 			if toolCalls, ok := m.parseToolCalls(sb.String()); ok {
 				resp.Message.ToolCalls = toolCalls
 				resp.Message.Content = ""
+				resp.DoneReason = "tool_calls"
 			}
 		}
 
