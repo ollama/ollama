1ca5c85415c9c359b07a0f60daea90eb9c8d94b0
Ryan Marten <ryanmarten2000@gmail.com>
2024-09-17 09:18:05 -0700

    consistent use of document instead of context and rewrite readme

--- examples/python-grounded-factuality-simple-check/main.py
+++ examples/python-grounded-factuality-simple-check/main.py
@@ -5,10 +5,10 @@ import requests
 
 # NOTE: ollama must be running for this to work, start the ollama app or run `ollama serve`
 
-def check(context, claim, model='jmorgan/bespoke-minicheck'):
-    """Checks if the claim is supported by the context by calling bespoke-minicheck.
+def check(document, claim, model='jmorgan/bespoke-minicheck'):
+    """Checks if the claim is supported by the  by calling bespoke-minicheck.
 
-    Returns Yes/yes if the claim is supported by the context, No/no otherwise.
+    Returns Yes/yes if the claim is supported by the document, No/no otherwise.
     Support for logits will be added in the future.
     
     bespoke-minicheck's system prompt is defined as:
@@ -18,9 +18,9 @@ def check(context, claim, model='jmorgan/bespoke-minicheck'):
       assess the claim's consistency with the document by responding with either "Yes" or "No".'
     
     bespoke-minicheck's user prompt is defined as:
-      "Document: {context}\nClaim: {claim}"
+      "Document: {document}\nClaim: {claim}"
     """
-    prompt = f"Document: {context}\nClaim: {claim}"
+    prompt = f"Document: {document}\nClaim: {claim}"
     r = requests.post(
         "http://0.0.0.0:11434/api/generate",
         json={"model": model,
@@ -52,13 +52,13 @@ def get_user_input(prompt):
 
 def main():
     while True:
-        # Get a context from the user (e.g. "Ryan likes running and biking.")
-        context = get_user_input("Enter a context: ")
+        # Get a document from the user (e.g. "Ryan likes running and biking.")
+        document = get_user_input("Enter a document: ")
         # Get a claim from the user (e.g. "Ryan likes to run.")
         claim = get_user_input("Enter a claim: ")
-        # Check if the claim is supported by the context
-        grounded_factuality_check = check(context, claim)
-        print(f"Is the claim supported by the context according to bespoke-minicheck? {grounded_factuality_check}")
+        # Check if the claim is supported by the document
+        grounded_factuality_check = check(document, claim)
+        print(f"Is the claim supported by the document according to bespoke-minicheck? {grounded_factuality_check}")
         print("\n\n")
 
 
--- examples/python-grounded-factuality-simple-check/readme.md
+++ examples/python-grounded-factuality-simple-check/readme.md
@@ -10,30 +10,45 @@
    ollama pull jmorgan/bespoke-minicheck
    ```
 
-2. Install the dependencies.
+2. Install the dependencies:
 
    ```bash
    pip install -r requirements.txt
    ```
 
-3. Run the example:
+3. Run the program:
 
    ```bash
-   python client.py
+   python main.py
    ```
 
-## Examples
-Document: 
+4. Enter a document and a claim when prompted:
+
+   ```bash
+   Enter a document: Roses are red.
+
+   Enter a claim: Roses are blue. 
+   ```
+
+   The claim and document are then given to the `bespoke-minicheck` as inputs, which then generates a response (Yes or No) on whether the claim is supported by the document.
+
+   ```bash
+   Is the claim supported by the document according to bespoke-minicheck? No
+   ```
+
+## More Examples
+
+Document ([source](https://en.wikipedia.org/wiki/Apple_I)): 
 > The Apple Computer 1 (Apple-1[a]), later known predominantly as the Apple I(written with a Roman numeral),[b] is an 8-bit motherboard-only personal computer designed by Steve Wozniak[5][6] and released by the Apple Computer Company (now Apple Inc.) in 1976. The company was initially formed to sell the Apple I – its first product – and would later become the world's largest technology company.[7] The idea of starting a company and selling the computer came from Wozniak's friend and Apple co-founder Steve Jobs.[8][9] One of the main innovations of the Apple I was that it included video display terminal circuitry on its circuit board, allowing it to connect to a low-cost composite video monitor or television, instead of an expensive computer terminal, compared to most existing computers at the time.
 
 Claim: 
 >The Apple I is a 16-bit computer.
 
 Expected output:
->Is the claim supported by the context according to bespoke-minicheck? No
+>Is the claim supported by the document according to bespoke-minicheck? No
 
 Claim: 
 >Apple was originally called the Apple Computer Company.
 
 Expected output:
->Is the claim supported by the context according to bespoke-minicheck? Yes
+>Is the claim supported by the document according to bespoke-minicheck? Yes
