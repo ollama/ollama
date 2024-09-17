95eafa8c850e188f1e5d9f80486161dba20c5faf
Ryan Marten <ryanmarten2000@gmail.com>
2024-09-17 13:21:34 -0700

    lint simple-check

--- examples/python-grounded-factuality-simple-check/main.py
+++ examples/python-grounded-factuality-simple-check/main.py
@@ -4,24 +4,28 @@ import ollama
 
 # NOTE: ollama must be running for this to work, start the ollama app or run `ollama serve`
 
-def check(document, claim, model='jmorgan/bespoke-minicheck'):
+
+def check(document, claim, model="jmorgan/bespoke-minicheck"):
     """Checks if the claim is supported by the document by calling bespoke-minicheck.
 
     Returns Yes/yes if the claim is supported by the document, No/no otherwise.
     Support for logits will be added in the future.
-    
+
     bespoke-minicheck's system prompt is defined as:
       'Determine whether the provided claim is consistent with the corresponding
       document. Consistency in this context implies that all information presented in the claim
-      is substantiated by the document. If not, it should be considered inconsistent. Please 
+      is substantiated by the document. If not, it should be considered inconsistent. Please
       assess the claim's consistency with the document by responding with either "Yes" or "No".'
-    
+
     bespoke-minicheck's user prompt is defined as:
       "Document: {document}\nClaim: {claim}"
     """
     prompt = f"Document: {document}\nClaim: {claim}"
-    response = ollama.generate(model=model, prompt=prompt, options={"num_predict": 2, "temperature": 0.0})
-    return response['response'].strip()
+    response = ollama.generate(
+        model=model, prompt=prompt, options={"num_predict": 2, "temperature": 0.0}
+    )
+    return response["response"].strip()
+
 
 def get_user_input(prompt):
     user_input = input(prompt)
@@ -29,6 +33,7 @@ def get_user_input(prompt):
         exit()
     print()
     return user_input
+
 
 def main():
     while True:
@@ -38,7 +43,9 @@ def main():
         claim = get_user_input("Enter a claim: ")
         # Check if the claim is supported by the document
         grounded_factuality_check = check(document, claim)
-        print(f"Is the claim supported by the document according to bespoke-minicheck? {grounded_factuality_check}")
+        print(
+            f"Is the claim supported by the document according to bespoke-minicheck? {grounded_factuality_check}"
+        )
         print("\n\n")
 
 
