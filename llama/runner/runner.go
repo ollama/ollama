94a48ab1b3ccdb3330d1e40ef6a1ad9b0f3710fb
Jesse Gross <jesse@ollama.com>
2024-09-20 16:49:29 -0700

    runner.go: Allocate batches for all sequences during init
    We should tell the model that we could have full batches for all
    sequences. We already do this when we allocate the batches but it was
    missed during initialization.

--- llama/runner/runner.go
+++ llama/runner/runner.go
@@ -655,7 +655,7 @@ func (s *Server) loadModel(
 
 	s.model = llama.LoadModelFromFile(mpath, params)
 
-	ctxParams := llama.NewContextParams(kvSize, s.batchSize, s.parallel, threads, flashAttention)
+	ctxParams := llama.NewContextParams(kvSize, s.batchSize*s.parallel, s.parallel, threads, flashAttention)
 	s.lc = llama.NewContextWithModel(s.model, ctxParams)
 
 	if lpath != "" {
