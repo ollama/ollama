ecf0de7ff03944c4cf890eb3655aee155db2f8a5
Daniel Hiltgen <daniel@ollama.com>
2024-09-26 15:21:33 -0700

    llama: wire up builtin runner
    This adds a new entrypoint into the ollama CLI to run the cgo built runner.
    On Mac arm64, this will have GPU support, but on all other platforms it will
    be the lowest common denominator CPU build.  After we fully transition
    to the new Go runners more tech-debt can be removed and we can stop building
    the "default" runner via make and rely on the builtin always.

--- main.go
+++ main.go
@@ -2,12 +2,21 @@ package main
 
 import (
 	"context"
+	"os"
 
 	"github.com/spf13/cobra"
 
 	"github.com/ollama/ollama/cmd"
+	"github.com/ollama/ollama/llama/runner"
 )
 
 func main() {
+	if len(os.Args) >= 2 {
+		if os.Args[1] == "_runner" {
+			os.Args = append([]string{os.Args[0]}, os.Args[2:]...)
+			runner.RunnerMain()
+			return
+		}
+	}
 	cobra.CheckErr(cmd.NewCLI().ExecuteContext(context.Background()))
 }
