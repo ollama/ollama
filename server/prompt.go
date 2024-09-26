96a8b2f7d8befee49c8a0c916c594e21f3de9a0c
Patrick Devine <patrick@infrahq.com>
2024-09-26 01:31:53 -0700

    fix prompt for non-mllama multimodal

--- server/prompt.go
+++ server/prompt.go
@@ -84,25 +84,25 @@ func chatPrompt(ctx context.Context, m *Model, tokenize tokenizeFunc, opts *api.
 			msgs[lastMsgIdx].Content = strings.TrimSpace("<|image|>" + msgs[lastMsgIdx].Content)
 			images = append(images, imgData)
 		}
-	}
+	} else {
+		for cnt, msg := range msgs[currMsgIdx:] {
+			for _, i := range msg.Images {
+				imgData := llm.ImageData{
+					ID:   len(images),
+					Data: i,
+				}
 
-	for cnt, msg := range msgs[currMsgIdx:] {
-		for _, i := range msg.Images {
-			imgData := llm.ImageData{
-				ID:   len(images),
-				Data: i,
+				imageTag := fmt.Sprintf("[img-%d]", imgData.ID)
+				prompt := msg.Content
+
+				if !strings.Contains(prompt, "[img]") {
+					prompt = strings.TrimSpace("[img] " + prompt)
+				}
+				prompt = strings.Replace(prompt, "[img]", imageTag, 1)
+				msgs[currMsgIdx+cnt].Content = prompt
+
+				images = append(images, imgData)
 			}
-
-			imageTag := fmt.Sprintf("[img-%d]", imgData.ID)
-			prompt := msg.Content
-
-			if !strings.Contains(prompt, "[img]") {
-				prompt = strings.TrimSpace("[img] " + prompt)
-			}
-			prompt = strings.Replace(prompt, "[img]", imageTag, 1)
-			msgs[currMsgIdx+cnt].Content = prompt
-
-			images = append(images, imgData)
 		}
 	}
 
