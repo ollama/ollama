0077e22d524edbad949002216f2ba6206aacb1b5
Jesse Gross <jesse@ollama.com>
2024-10-09 16:12:23 -0700

    runner.go: Handle truncation of tokens for stop sequences
    When a single token contains both text to be return and a stop
    sequence, this causes an out of bounds error when we update the
    cache to match our text. This is because we currently assume that
    the removing the stop sequence will consume at least one token.
    
    This also inverts the logic to deal with positive numbers, rather
    than a value to be subtracted, which is easier to reason about.
    
    Fixes #7153

--- llama/runner/stop_test.go
+++ llama/runner/stop_test.go
@@ -7,42 +7,54 @@ import (
 
 func TestTruncateStop(t *testing.T) {
 	tests := []struct {
-		name     string
-		pieces   []string
-		stop     string
-		expected []string
+		name          string
+		pieces        []string
+		stop          string
+		expected      []string
+		expectedTrunc bool
 	}{
 		{
-			name:     "Single word",
-			pieces:   []string{"hello", "world"},
-			stop:     "world",
-			expected: []string{"hello"},
+			name:          "Single word",
+			pieces:        []string{"hello", "world"},
+			stop:          "world",
+			expected:      []string{"hello"},
+			expectedTrunc: false,
 		},
 		{
-			name:     "Partial",
-			pieces:   []string{"hello", "wor"},
-			stop:     "or",
-			expected: []string{"hello", "w"},
+			name:          "Partial",
+			pieces:        []string{"hello", "wor"},
+			stop:          "or",
+			expected:      []string{"hello", "w"},
+			expectedTrunc: true,
 		},
 		{
-			name:     "Suffix",
-			pieces:   []string{"Hello", " there", "!"},
-			stop:     "!",
-			expected: []string{"Hello", " there"},
+			name:          "Suffix",
+			pieces:        []string{"Hello", " there", "!"},
+			stop:          "!",
+			expected:      []string{"Hello", " there"},
+			expectedTrunc: false,
 		},
 		{
-			name:     "Middle",
-			pieces:   []string{"hello", " wor"},
-			stop:     "llo w",
-			expected: []string{"he"},
+			name:          "Suffix partial",
+			pieces:        []string{"Hello", " the", "re!"},
+			stop:          "there!",
+			expected:      []string{"Hello", " "},
+			expectedTrunc: true,
+		},
+		{
+			name:          "Middle",
+			pieces:        []string{"hello", " wor"},
+			stop:          "llo w",
+			expected:      []string{"he"},
+			expectedTrunc: true,
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			result := truncateStop(tt.pieces, tt.stop)
-			if !reflect.DeepEqual(result, tt.expected) {
-				t.Errorf("truncateStop(%v, %s): have %v; want %v", tt.pieces, tt.stop, result, tt.expected)
+			result, resultTrunc := truncateStop(tt.pieces, tt.stop)
+			if !reflect.DeepEqual(result, tt.expected) || resultTrunc != tt.expectedTrunc {
+				t.Errorf("truncateStop(%v, %s): have %v (%v); want %v (%v)", tt.pieces, tt.stop, result, resultTrunc, tt.expected, tt.expectedTrunc)
 			}
 		})
 	}
