054e31146ef7ee33a82ac3c870b71f93791c4cbe
Michael Yang <mxyng@pm.me>
2024-12-09 15:38:39 -0800

    build: recursive make ggml-cuda

--- ml/backend/ggml/ggml/ggml.go
+++ ml/backend/ggml/ggml/ggml.go
@@ -5,8 +5,7 @@ package ggml
 // #cgo CPPFLAGS: -DNDEBUG -DGGML_USE_CPU
 // #cgo darwin LDFLAGS: -framework Foundation
 // #cgo amd64,avx CPPFLAGS: -mavx
-// #cgo amd64,avx2 CPPFLAGS: -mavx2 -mfma
-// #cgo amd64,f16c CPPFLAGS: -mf16c
+// #cgo amd64,avx2 CPPFLAGS: -mavx2 -mfma -mf16c
 // #cgo arm64 CPPFLAGS: -D__aarch64__ -D__ARM_NEON -D__ARM_FEATURE_FMA
 import "C"
 import _ "github.com/ollama/ollama/ml/backend/ggml/ggml/ggml-cpu"
