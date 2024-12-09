eaa160283e8bebc147a810762cbccb94418c9c0f
Michael Yang <mxyng@pm.me>
2024-12-09 15:38:39 -0800

    build: recursive make ggml-cuda

--- ml/backend/ggml/ggml/ggml-cuda/cuda.go
+++ ml/backend/ggml/ggml/ggml-cuda/cuda.go
@@ -1,3 +1,7 @@
 package cuda
 
+// #cgo cuda_v11 LDFLAGS: -L. -lggml_cuda_v11
+// #cgo cuda_v12 LDFLAGS: -L. -lggml_cuda_v12
+// #cgo cuda_v11 cuda_v12 LDFLAGS: -L/usr/local/cuda/lib64 -lcuda -lcudart -lcublas -lcublasLt
+// #cgo rocm LDFLAGS: -L. -lggml_rocm -L/opt/rocm/lib -lhipblas -lamdhip64 -lrocblas
 import "C"
--- ml/backend/ggml/ggml/ggml_cuda.go
+++ ml/backend/ggml/ggml/ggml_cuda.go
@@ -3,5 +3,6 @@
 package ggml
 
 // #cgo CPPFLAGS: -DGGML_USE_CUDA
+// #cgo rocm CPPFLAGS: -DGGML_USE_HIP
 import "C"
 import _ "github.com/ollama/ollama/ml/backend/ggml/ggml/ggml-cuda"
