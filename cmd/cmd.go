b0087f990d742a0f02a47a68886ef1a8a233edd1
Bernhard Froemel <bf@ctsw.at>
2024-12-21 13:31:37 +0100

    cmd/cmd.go: removed obsolete OLLAMA_DRAFT_* environment variables

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1473,8 +1473,6 @@ func NewCLI() *cobra.Command {
 				envVars["OLLAMA_LLM_LIBRARY"],
 				envVars["OLLAMA_GPU_OVERHEAD"],
 				envVars["OLLAMA_LOAD_TIMEOUT"],
-				envVars["OLLAMA_DRAFT_GPU_DEVS"],
-				envVars["OLLAMA_DRAFT_GPU_LAYERS"],
 			})
 		default:
 			appendEnvDocs(cmd, envs)
