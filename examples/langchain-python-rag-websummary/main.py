c4a3effea6f63a2c5d2764e4aa14b1ca6aa27fe6
Talen-520 <tao727188712@gmail.com>
2025-01-06 00:34:18 -0500

    refactor: update import for Ollama and modify requirements for langchain-ollama

--- examples/langchain-python-rag-websummary/main.py
+++ examples/langchain-python-rag-websummary/main.py
@@ -1,11 +1,11 @@
-from langchain_community.llms import Ollama
+from langchain_ollama.llms import OllamaLLM
 from langchain_community.document_loaders import WebBaseLoader
 from langchain.chains.summarize import load_summarize_chain
 
 loader = WebBaseLoader("https://ollama.com/blog/run-llama2-uncensored-locally")
 docs = loader.load()
 
-llm = Ollama(model="llama3.2")
+llm = OllamaLLM(model="llama3.2")
 chain = load_summarize_chain(llm, chain_type="stuff")
 
 result = chain.invoke(docs)
