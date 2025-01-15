2539f2dbf99ec1b8f44ece884bf2c8678fca3127
Patrick Devine <patrick@infrahq.com>
2025-01-14 19:01:24 -0800

    Fix absolute path names + gguf detection (#8428)

--- parser/parser.go
+++ parser/parser.go
@@ -564,7 +564,9 @@ func isValidCommand(cmd string) bool {
 }
 
 func expandPathImpl(path, relativeDir string, currentUserFunc func() (*user.User, error), lookupUserFunc func(string) (*user.User, error)) (string, error) {
-	if strings.HasPrefix(path, "~") {
+	if filepath.IsAbs(path) || strings.HasPrefix(path, "\\") || strings.HasPrefix(path, "/") {
+		return filepath.Abs(path)
+	} else if strings.HasPrefix(path, "~") {
 		var homeDir string
 
 		if path == "~" || strings.HasPrefix(path, "~/") {
