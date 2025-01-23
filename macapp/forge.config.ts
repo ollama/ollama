7ba818748d105d8f9f1213c761563ee6915eca1f
jmorganca <jmorganca@gmail.com>
2025-01-23 14:15:46 -0800

    update build_darwin.sh

--- macapp/forge.config.ts
+++ macapp/forge.config.ts
@@ -18,8 +18,8 @@ const config: ForgeConfig = {
     asar: true,
     icon: './assets/icon.icns',
     extraResource: [
-      '../dist/ollama',
-      '../dist/darwin-amd64/lib',
+      path.join(__dirname, '../dist/ollama'),
+      ...fs.readdirSync(path.join(__dirname, '../build/lib')).map(f => path.join(__dirname, '../build/lib', f)),
       path.join(__dirname, './assets/iconTemplate.png'),
       path.join(__dirname, './assets/iconTemplate@2x.png'),
       path.join(__dirname, './assets/iconUpdateTemplate.png'),
@@ -43,7 +43,7 @@ const config: ForgeConfig = {
         }
       : {}),
     osxUniversal: {
-      x64ArchFiles: '**/ollama*',
+      x64ArchFiles: '*',
     },
   },
   rebuildConfig: {},
--- scripts/build_darwin.sh
+++ scripts/build_darwin.sh
@@ -10,17 +10,16 @@ mkdir -p dist
 # If installed to an alternate location use the following to enable
 # export SDKROOT=/Applications/Xcode_12.5.1.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk
 # export DEVELOPER_DIR=/Applications/Xcode_12.5.1.app/Contents/Developer
+export CGO_LDFLAGS=""
 export CGO_CPPFLAGS=-mmacosx-version-min=11.3
-
-rm -rf llama/build dist/darwin-*
 
 # Generate the universal ollama binary for stand-alone usage: metal + avx
 echo "Building binary"
 echo "Building darwin arm64"
-GOOS=darwin ARCH=arm64 GOARCH=arm64 make -j 8 dist
+GOOS=darwin GOARCH=arm64 CGO_ENABLED=1 go build -o dist/ollama-darwin-arm64
 echo "Building darwin amd64 with AVX enabled"
-GOOS=darwin ARCH=amd64 GOARCH=amd64 CUSTOM_CPU_FLAGS="avx" make -j 8 dist_exe
-lipo -create -output dist/ollama-darwin dist/darwin-arm64/bin/ollama dist/darwin-amd64/bin/ollama
+GOOS=darwin GOARCH=amd64 CGO_ENABLED=1 GO_CPPFLAGS="-DGGML_AVX" go build -o dist/ollama-darwin-amd64
+lipo -create -output dist/ollama-darwin dist/ollama-darwin-arm64 dist/ollama-darwin-amd64
 
 # sign the binary and rename it
 if [ -n "$APPLE_IDENTITY" ]; then
@@ -33,15 +32,18 @@ if [ -n "$APPLE_IDENTITY" ]; then
     xcrun notarytool submit dist/temp.zip --wait --timeout 10m --apple-id $APPLE_ID --password $APPLE_PASSWORD --team-id $APPLE_TEAM_ID
 fi
 rm -f dist/temp.zip
+cp dist/ollama-darwin dist/ollama
 
-# Build the app bundle
-echo "Building app"
-echo "Building darwin amd64 with runners"
-rm dist/darwin-amd64/bin/ollama
-GOOS=darwin ARCH=amd64 GOARCH=amd64 make -j 8 dist
-
-# Generate the universal ollama binary for the app bundle: metal + no-avx
-lipo -create -output dist/ollama dist/darwin-arm64/bin/ollama dist/darwin-amd64/bin/ollama
+cmake -B build -DCMAKE_OSX_DEPLOYMENT_TARGET=11.3 -DCMAKE_SYSTEM_PROCESSOR=x86_64 -DCMAKE_OSX_ARCHITECTURES=x86_64
+cmake --build build -j
+if [ -n "$APPLE_IDENTITY" ]; then
+    for lib in build/lib/*; do
+        echo "Signing $lib"
+        codesign -f --timestamp -s "$APPLE_IDENTITY" --identifier ai.ollama.ollama --options=runtime "$lib"
+    done
+else
+    echo "WARNING: Skipping library code signing - set APPLE_IDENTITY" 
+fi
 
 # build and optionally sign the mac app
 npm install --prefix macapp
@@ -50,5 +52,4 @@ if [ -n "$APPLE_IDENTITY" ]; then
 else 
     npm run --prefix macapp make
 fi
-cp macapp/out/make/zip/darwin/universal/Ollama-darwin-universal-$VERSION.zip dist/Ollama-darwin.zip
-
+cp ./macapp/out/make/zip/darwin/universal/Ollama-darwin-universal-$VERSION.zip dist/Ollama-darwin.zip
