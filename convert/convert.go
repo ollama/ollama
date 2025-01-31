144a20369a4c7136f0aab500f4704a8b1bc615ce
Yash Khare <khareyash05@gmail.com>
2025-01-31 16:26:09 +0530

    revert file

--- convert/convert.go
+++ convert/convert.go
@@ -138,6 +138,8 @@ func ConvertAdapter(fsys fs.FS, ws io.WriteSeeker, baseKV llm.KV) error {
 	switch arch {
 	case "llama":
 		conv = &llamaAdapter{}
+	case "gemma2":
+		conv = &gemma2Adapter{}
 	default:
 		return errors.New("unsupported architecture")
 	}
@@ -179,10 +181,18 @@ func ConvertModel(fsys fs.FS, ws io.WriteSeeker) error {
 		conv = &llamaModel{}
 	case "MixtralForCausalLM":
 		conv = &mixtralModel{}
+	case "GemmaForCausalLM":
+		conv = &gemmaModel{}
+	case "Gemma2ForCausalLM":
+		conv = &gemma2Model{}
 	case "Phi3ForCausalLM":
 		conv = &phi3Model{}
 	case "Qwen2ForCausalLM":
 		conv = &qwen2Model{}
+	case "BertModel":
+		conv = &bertModel{}
+	case "CohereForCausalLM":
+		conv = &commandrModel{}
 	default:
 		return errors.New("unsupported architecture")
 	}
