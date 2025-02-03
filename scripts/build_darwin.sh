669dc31cf33a1d167dbbe5398e3e2ec71620bcef
Michael Yang <mxyng@pm.me>
2025-02-03 13:31:22 -0800

    fix build

--- scripts/build_darwin.sh
+++ scripts/build_darwin.sh
@@ -52,7 +52,7 @@ _sign_darwin() {
         # create a temporary zip for notarization
         TEMP=$(mktemp -u).zip
         ditto -c -k --keepParent dist/darwin/ollama "$TEMP"
-        xcrun notarytool submit dist/darwin/temp.zip --wait --timeout 10m --apple-id $APPLE_ID --password $APPLE_PASSWORD --team-id $APPLE_TEAM_ID
+        xcrun notarytool submit "$TEMP" --wait --timeout 10m --apple-id $APPLE_ID --password $APPLE_PASSWORD --team-id $APPLE_TEAM_ID
         rm -f "$TEMP"
     fi
 
