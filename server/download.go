41d3e8c9b17c345520fb813330375935d386629b
jmorganca <jmorganca@gmail.com>
2025-02-04 19:10:06 -0800

    server: always print upload/download part info

--- server/download.go
+++ server/download.go
@@ -170,9 +170,12 @@ func (b *blobDownload) Prepare(ctx context.Context, requestURL *url.URL, opts *r
 
 			offset += size
 		}
-	} else {
+	}
+
+	if len(b.Parts) > 0 {
 		slog.Info(fmt.Sprintf("downloading %s in %d %s part(s)", b.Digest[7:19], len(b.Parts), format.HumanBytes(b.Parts[0].Size)))
 	}
+
 	return nil
 }
 
