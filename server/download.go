d8932c55e72f919cd92d0d019fb7fd0e01d857cd
William <wllm.francis@gmail.com>
2025-02-04 21:52:47 -0500

    server: fix out of bounds exception on model download (#8746)

--- server/download.go
+++ server/download.go
@@ -170,9 +170,9 @@ func (b *blobDownload) Prepare(ctx context.Context, requestURL *url.URL, opts *r
 
 			offset += size
 		}
+	} else {
+		slog.Info(fmt.Sprintf("downloading %s in %d %s part(s)", b.Digest[7:19], len(b.Parts), format.HumanBytes(b.Parts[0].Size)))
 	}
-
-	slog.Info(fmt.Sprintf("downloading %s in %d %s part(s)", b.Digest[7:19], len(b.Parts), format.HumanBytes(b.Parts[0].Size)))
 	return nil
 }
 
