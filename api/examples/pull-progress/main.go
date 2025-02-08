819c391f3dae579d7e99cb892fb0308a77f727be
Yash Khare <khareyash05@gmail.com>
2025-02-08 12:42:01 +0530

    remove indentation issue with functions
    Signed-off-by: Yash Khare <khareyash05@gmail.com>

--- api/examples/pull-progress/main.go
+++ api/examples/pull-progress/main.go
@@ -1,36 +1,31 @@
 package main
 
 import (
-    "context"
-    "fmt"
-    "log"
+	"context"
+	"fmt"
+	"log"
 
-    "github.com/ollama/ollama/api"
+	"github.com/ollama/ollama/api"
 )
 
-// Refactored to inject the client dependency for better testability.
 func main() {
-    client, err := api.ClientFromEnvironment()
-    if err != nil {
-        log.Fatal(err)
-    }
+	client, err := api.ClientFromEnvironment()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    runPullProcess(client)
-}
+	ctx := context.Background()
 
-func runPullProcess(client *api.Client) {
-    ctx := context.Background()
+	req := &api.PullRequest{
+		Model: "mistral",
+	}
+	progressFunc := func(resp api.ProgressResponse) error {
+		fmt.Printf("Progress: status=%v, total=%v, completed=%v\n", resp.Status, resp.Total, resp.Completed)
+		return nil
+	}
 
-    req := &api.PullRequest{
-        Model: "mistral",
-    }
-    progressFunc := func(resp api.ProgressResponse) error {
-        fmt.Printf("Progress: status=%v, total=%v, completed=%v\n", resp.Status, resp.Total, resp.Completed)
-        return nil
-    }
-
-    err := client.Pull(ctx, req, progressFunc)
-    if err != nil {
-        log.Fatal(err)
-    }
+	err = client.Pull(ctx, req, progressFunc)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
