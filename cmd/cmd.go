1deacab6d1b76d8f7f02e797908e42a7e03f4b96
Richard Lyons <frob@cloudstaff.com>
2025-02-13 18:30:43 +0100

    Document missing Modelfile.

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1165,13 +1165,13 @@ func NewCLI() *cobra.Command {
 
 	createCmd := &cobra.Command{
 		Use:     "create MODEL",
-		Short:   "Create a model from a Modelfile",
+		Short:   "Create a model from a Modelfile or the current directory (same as \"FROM .\")",
 		Args:    cobra.ExactArgs(1),
 		PreRunE: checkServerHeartbeat,
 		RunE:    CreateHandler,
 	}
 
-	createCmd.Flags().StringP("file", "f", "", "Name of the Modelfile (default \"Modelfile\"")
+	createCmd.Flags().StringP("file", "f", "", "Name of the Modelfile (default \"Modelfile\")")
 	createCmd.Flags().StringP("quantize", "q", "", "Quantize model to this level (e.g. q4_0)")
 
 	showCmd := &cobra.Command{
