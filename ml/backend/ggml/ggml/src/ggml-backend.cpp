a2bbd7bbb923355682d3394a7da871d3bda5557b
hackhy <hackhy@163.com>
2025-02-17 11:25:03 +0800

    Update ggml-backend.cpp
    Change MAX GPU+CPU from 16 to 64

--- ml/backend/ggml/ggml/src/ggml-backend.cpp
+++ ml/backend/ggml/ggml/src/ggml-backend.cpp
@@ -606,7 +606,7 @@ static bool ggml_is_view_op(enum ggml_op op) {
 // scheduler
 
 #ifndef GGML_SCHED_MAX_BACKENDS
-#define GGML_SCHED_MAX_BACKENDS 16
+#define GGML_SCHED_MAX_BACKENDS 64
 #endif
 
 #ifndef GGML_SCHED_MAX_SPLIT_INPUTS
