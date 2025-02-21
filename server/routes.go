6b6746b5b49e41120eb664bcc95cdf8bdb64d68c
Richard Lyons <frob@cloudstaff.com>
2025-02-21 18:41:34 +0100

    server: emit load and total durations for a load

--- server/routes.go
+++ server/routes.go
@@ -191,6 +191,10 @@ func (s *Server) GenerateHandler(c *gin.Context) {
 			CreatedAt:  time.Now().UTC(),
 			Done:       true,
 			DoneReason: "load",
+			Metrics: api.Metrics{
+				LoadDuration:  checkpointLoaded.Sub(checkpointStart),
+				TotalDuration: checkpointLoaded.Sub(checkpointStart),
+			},
 		})
 		return
 	}
@@ -1443,6 +1447,10 @@ func (s *Server) ChatHandler(c *gin.Context) {
 			Message:    api.Message{Role: "assistant"},
 			Done:       true,
 			DoneReason: "load",
+			Metrics: api.Metrics{
+				LoadDuration:  checkpointLoaded.Sub(checkpointStart),
+				TotalDuration: checkpointLoaded.Sub(checkpointStart),
+			},
 		})
 		return
 	}
