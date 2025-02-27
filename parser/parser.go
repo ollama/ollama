0749160a245e653ac9d4095e7f6c1833a813ffbb
Xavier Rey-Robert @ IBM <43029233+XReyRobert-IBM@users.noreply.github.com>
2025-02-27 15:23:20 +0100

    Update parser.go
    Added "control" role to the list of valid roles. (Granite3.2 support)

--- parser/parser.go
+++ parser/parser.go
@@ -557,7 +557,7 @@ func isNewline(r rune) bool {
 }
 
 func isValidMessageRole(role string) bool {
-	return role == "system" || role == "user" || role == "assistant"
+	return role == "system" || role == "user" || role == "assistant" || role == "control"
 }
 
 func isValidCommand(cmd string) bool {
