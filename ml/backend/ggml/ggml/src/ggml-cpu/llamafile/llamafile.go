ebdcbc2187e5425be8b3086ca4f740a476a447d8
amritahs <amritahs@linux.vnet.ibm.com>
2025-02-27 01:02:14 -0600

    powerpc: Add -mcpu=power10 flag when compiling llamafile folder
    -mcpu=power10 needs to be added in llamafile.go, so
    that powerpc optimized code(using Matrix Multiply
    Assist) for llamafile_sgemm is enabled and is
    available in ollama binary.
    
    Signed-off-by: amritahs <amritahs@linux.vnet.ibm.com>

--- ml/backend/ggml/ggml/src/ggml-cpu/llamafile/llamafile.go
+++ ml/backend/ggml/ggml/src/ggml-cpu/llamafile/llamafile.go
@@ -1,5 +1,5 @@
 package llamafile
 
-// #cgo CXXFLAGS: -std=c++17
+// #cgo CXXFLAGS: -std=c++17 -mcpu=power10
 // #cgo CPPFLAGS: -I${SRCDIR}/.. -I${SRCDIR}/../.. -I${SRCDIR}/../../../include
 import "C"
