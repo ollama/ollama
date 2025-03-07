444e994ccdde822b4e8b5d2e129aaf71aded4ab6
Gabe Goodhart <ghart@us.ibm.com>
2025-03-07 16:15:03 -0700

    feat: First cut at adding "granite" model support
    Branch: GraniteNewEngine
    
    Signed-off-by: Gabe Goodhart <ghart@us.ibm.com>

--- model/models/models.go
+++ model/models/models.go
@@ -3,6 +3,7 @@ package models
 import (
 	_ "github.com/ollama/ollama/model/models/gemma2"
 	_ "github.com/ollama/ollama/model/models/gemma3"
+	_ "github.com/ollama/ollama/model/models/granite"
 	_ "github.com/ollama/ollama/model/models/llama"
 	_ "github.com/ollama/ollama/model/models/mllama"
 )
