a75703b2cc196e97b535e5d66ff635ab5a726582
jmorganca <jmorganca@gmail.com>
2025-03-16 13:30:12 -0400

    wip

--- convert/convert.go
+++ convert/convert.go
@@ -182,8 +182,10 @@ func ConvertModel(fsys fs.FS, ws io.WriteSeeker) error {
 
 	var conv ModelConverter
 	switch p.Architectures[0] {
-	case "LlamaForCausalLM", "MistralForCausalLM":
+	case "LlamaForCausalLM":
 		conv = &llamaModel{}
+	case "MistralForCausalLM":
+		conv = &mistralModel{}
 	case "MixtralForCausalLM":
 		conv = &mixtralModel{}
 	case "GemmaForCausalLM":
