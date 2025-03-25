76fb0320ab1b8a7922bd17799ceb5b27c3e22076
Gabe Goodhart <ghart@us.ibm.com>
2025-03-25 12:33:24 -0600

    feat: Add support for multi-regex pretokenizers in BytePairEncoding
    Branch: GraniteNewEngine
    
    Signed-off-by: Gabe Goodhart <ghart@us.ibm.com>

--- model/process_text.go
+++ model/process_text.go
@@ -110,13 +110,24 @@ func (v *Vocabulary) Merge(left, right string) int {
 }
 
 type BytePairEncoding struct {
-	pre   *regexp2.Regexp
+	pre   []*regexp2.Regexp
 	vocab *Vocabulary
 }
 
 func NewBytePairEncoding(pre string, vocab *Vocabulary) BytePairEncoding {
 	return BytePairEncoding{
-		pre:   regexp2.MustCompile(pre, regexp2.Unicode|regexp2.RE2),
+		pre:   []*regexp2.Regexp{regexp2.MustCompile(pre, regexp2.Unicode|regexp2.RE2)},
+		vocab: vocab,
+	}
+}
+
+func NewMultiRegexBytePairEncoding(pres []string, vocab *Vocabulary) BytePairEncoding {
+	preExprs := []*regexp2.Regexp{}
+	for _, pre := range pres {
+		preExprs = append(preExprs, regexp2.MustCompile(pre, regexp2.Unicode|regexp2.RE2))
+	}
+	return BytePairEncoding{
+		pre:   preExprs,
 		vocab: vocab,
 	}
 }
@@ -127,8 +138,18 @@ func (bpe BytePairEncoding) Is(id int32, special Special) bool {
 
 func (bpe *BytePairEncoding) split(s string) iter.Seq[string] {
 	return func(yield func(string) bool) {
-		for m, _ := bpe.pre.FindStringMatch(s); m != nil; m, _ = bpe.pre.FindNextMatch(m) {
-			if !yield(m.String()) {
+		chunks := []string{s}
+		for _, pre := range bpe.pre {
+			prevChunks := chunks
+			chunks = []string{}
+			for _, chunk := range prevChunks {
+				for m, _ := pre.FindStringMatch(chunk); m != nil; m, _ = pre.FindNextMatch(m) {
+					chunks = append(chunks, m.String())
+				}
+			}
+		}
+		for _, chunk := range chunks {
+			if !yield(chunk) {
 				break
 			}
 		}
