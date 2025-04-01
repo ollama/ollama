90d8a1e8a2ef275244239ea5683815dc4e2b9fdf
Michael Yang <git@mxy.ng>
2025-04-01 13:40:36 -0700

    mistral-small: use ollama engine

--- fs/ggml/ggml.go
+++ fs/ggml/ggml.go
@@ -134,7 +134,10 @@ func (kv KV) Floats(key string, defaultValue ...[]float32) []float32 {
 }
 
 func (kv KV) OllamaEngineRequired() bool {
-	return kv.Architecture() == "gemma3"
+	return slices.Contains([]string{
+		"gemma3",
+		"mistral3",
+	}, kv.Architecture())
 }
 
 func keyValue[T string | uint32 | uint64 | float32 | *array | bool](kv KV, key string, defaultValue ...T) T {
