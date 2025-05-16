5b971ff9ab14800cbfe24aecf0cf87c9da3cf0d2
AJ Roetker <aj.roetker@contentstack.com>
2025-05-15 22:32:51 -0700

    Add ability to generate multimodal embeddings via embeddings endpoints

--- api/types.go
+++ api/types.go
@@ -290,7 +290,8 @@ type EmbedRequest struct {
 	Model string `json:"model"`
 
 	// Input is the input to embed.
-	Input any `json:"input"`
+	Input  any         `json:"input"`
+	Images []ImageData `json:"images,omitempty"`
 
 	// KeepAlive controls how long the model will stay loaded in memory following
 	// this request.
@@ -318,7 +319,8 @@ type EmbeddingRequest struct {
 	Model string `json:"model"`
 
 	// Prompt is the textual prompt to embed.
-	Prompt string `json:"prompt"`
+	Prompt string    `json:"prompt"`
+	Image  ImageData `json:"image,omitempty"`
 
 	// KeepAlive controls how long the model will stay loaded in memory following
 	// this request.
