f234ac1e51f672e883f49574b44bc5f0221045c8
Anuraag Agrawal <anuraaga@gmail.com>
2025-05-23 14:02:57 +0900

    cmd: add command to print third-party licenses

---
+++ licenses/licenses.go
@@ -0,0 +1,32 @@
+// Package licenses embeds third-party licenses for release binaries.
+// The licenses can be collected for embedding using
+//
+//	go run github.com/google/go-licenses@v1.6.0 save . --save_path=licenses/content
+package licenses
+
+import (
+	"embed"
+	"io/fs"
+)
+
+//go:embed content
+var licenses embed.FS
+
+var LicenseText string
+
+func init() {
+	fs.WalkDir(licenses, ".", func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if d.IsDir() {
+			return nil
+		}
+		content, err := fs.ReadFile(licenses, path)
+		if err != nil {
+			return err
+		}
+		LicenseText += string(content) + "\n"
+		return nil
+	})
+}
