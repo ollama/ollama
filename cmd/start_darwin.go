a8ed68bd9383ffec346fd1b3cf60d94c032bbec8
Daniel Hiltgen <dhiltgen@users.noreply.github.com>
2025-06-06 14:06:29 -0700

    launch app hidden (#10962)
    When starting the app in the background, start it hidden.

--- cmd/start_darwin.go
+++ cmd/start_darwin.go
@@ -23,7 +23,7 @@ func startApp(ctx context.Context, client *api.Client) error {
 		return errors.New("could not find ollama app")
 	}
 	path := strings.Split(link, "Ollama.app")
-	if err := exec.Command("/usr/bin/open", "-a", path[0]+"Ollama.app").Run(); err != nil {
+	if err := exec.Command("/usr/bin/open", "-j", "-a", path[0]+"Ollama.app").Run(); err != nil {
 		return err
 	}
 	return waitForServer(ctx, client)
