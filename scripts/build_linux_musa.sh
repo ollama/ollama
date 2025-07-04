6b0600118a01da2e09e07413885aecb795cbc0ce
R0CKSTAR <yeahdongcn@gmail.com>
2025-07-04 11:11:45 +0800

    hack: update version for building docker image and tgz (#11)
    Signed-off-by: Xiaodong Ye <xiaodong.ye@mthreads.com>

--- scripts/build_linux_musa.sh
+++ scripts/build_linux_musa.sh
@@ -20,7 +20,7 @@ echo "TARGET: $TARGET"
 echo "Platform: $OS/$ARCH"
 
 # Set up environment variables (similar to setup-environment job)
-VERSION=${VERSION:-$(git describe --tags --first-parent --abbrev=7 --long --dirty --always | sed -e "s/^v//g")}
+VERSION=${VERSION:-$(git describe --tags --first-parent --abbrev=7 --always | sed -e "s/^v//g")}
 GOFLAGS="'-ldflags=-w -s \"-X=github.com/ollama/ollama/version.Version=$VERSION\" \"-X=github.com/ollama/ollama/server.mode=release\"'"
 
 echo "VERSION: $VERSION"
--- scripts/env.sh
+++ scripts/env.sh
@@ -1,6 +1,6 @@
 # Common environment setup across build*.sh scripts
 
-export VERSION=${VERSION:-$(git describe --tags --first-parent --abbrev=7 --long --dirty --always | sed -e "s/^v//g")}
+export VERSION=${VERSION:-$(git describe --tags --first-parent --abbrev=7 --always | sed -e "s/^v//g")}
 export GOFLAGS="'-ldflags=-w -s \"-X=github.com/ollama/ollama/version.Version=$VERSION\" \"-X=github.com/ollama/ollama/server.mode=release\"'"
 # TODO - consider `docker buildx ls --format=json` to autodiscover platform capability
 PLATFORM=${PLATFORM:-"linux/arm64,linux/amd64"}
