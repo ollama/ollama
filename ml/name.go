5a028850881eee85dce3db0ae3c00a3dde431684
gr4ceG <xiangstong@gmail.com>
2025-09-22 01:48:22 -0700

    whoops

---
+++ ml/name.go
@@ -0,0 +1,8 @@
+package ml
+
+// lets callers do ml.SetName(t, "foo") without importing ggml
+func SetName(t Tensor, name string) {
+  if n, ok := t.(interface{ SetName(string) }); ok {
+    n.SetName(name)
+  }
+}
