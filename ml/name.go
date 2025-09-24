4661f4e298af8cfde69b18d7ce18e6fb52657348
gr4ceG <xiangstong@gmail.com>
2025-09-23 18:21:34 -0700

    remove name go

--- ml/name.go
+++
@@ -1,8 +0,0 @@
-package ml
-
-// lets callers do ml.SetName(t, "foo") without importing ggml
-func SetName(t Tensor, name string) {
-	if n, ok := t.(interface{ SetName(string) }); ok {
-		n.SetName(name)
-	}
-}
