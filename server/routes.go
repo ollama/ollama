d98b996e9bfa0ca9ecd76e774f558c137cbf6283
ParthSareen <parth.sareen@ollama.com>
2025-10-22 13:52:22 -0700

    linter

--- server/routes.go
+++ server/routes.go
@@ -2173,7 +2173,6 @@ func (s *Server) ChatHandler(c *gin.Context) {
 						}
 						res.Message.ToolCalls = toolCalls
 						res.Message.Content = ""
-
 					} else if res.Message.Thinking != "" {
 						// don't return
 					} else {
