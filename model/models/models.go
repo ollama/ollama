b4848fec6a643a4c33c4b4f10842a7c132f1dde2
nicole pardal <nicolepardall@gmail.com>
2025-11-13 15:54:58 -0800

    lint

--- model/models/models.go
+++ model/models/models.go
@@ -2,7 +2,6 @@ package models
 
 import (
 	_ "github.com/ollama/ollama/model/models/bert"
-	_ "github.com/ollama/ollama/model/models/nomicbert"
 	_ "github.com/ollama/ollama/model/models/deepseek2"
 	_ "github.com/ollama/ollama/model/models/gemma2"
 	_ "github.com/ollama/ollama/model/models/gemma3"
@@ -12,6 +11,7 @@ import (
 	_ "github.com/ollama/ollama/model/models/llama4"
 	_ "github.com/ollama/ollama/model/models/mistral3"
 	_ "github.com/ollama/ollama/model/models/mllama"
+	_ "github.com/ollama/ollama/model/models/nomicbert"
 	_ "github.com/ollama/ollama/model/models/qwen2"
 	_ "github.com/ollama/ollama/model/models/qwen25vl"
 	_ "github.com/ollama/ollama/model/models/qwen3"
