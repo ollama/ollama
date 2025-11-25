a60361082083943913e3c873b467227a53baed3b
nicole pardal <nicolepardall@gmail.com>
2025-11-25 15:49:09 -0800

    olmo model initial

---
+++ convert/convert_olmo.go
@@ -0,0 +1,82 @@
+package convert
+
+import (
+	"cmp"
+
+	"github.com/ollama/ollama/fs/ggml"
+)
+
+type olmoModel struct {
+	ModelParameters
+
+	HiddenSize            uint32  `json:"hidden_size"`
+	NumHiddenLayers       uint32  `json:"num_hidden_layers"`
+	IntermediateSize      uint32  `json:"intermediate_size"`
+	NumAttentionHeads     uint32  `json:"num_attention_heads"`
+	NumKeyValueHeads      uint32  `json:"num_key_value_heads"`
+	MaxPositionEmbeddings uint32  `json:"max_position_embeddings"`
+	RMSNormEPS            float32 `json:"rms_norm_eps"`
+	RopeTheta             float32 `json:"rope_theta"`
+	ClampKQV              float32 `json:"f_clamp_kqv"`
+}
+
+var _ ModelConverter = (*olmoModel)(nil)
+
+func (p *olmoModel) KV(t *Tokenizer) ggml.KV {
+	kv := p.ModelParameters.KV(t)
+	kv["general.architecture"] = "olmo"
+	kv["olmo.block_count"] = p.NumHiddenLayers
+	kv["olmo.context_length"] = p.MaxPositionEmbeddings
+	kv["olmo.embedding_length"] = p.HiddenSize
+	kv["olmo.feed_forward_length"] = p.IntermediateSize
+	kv["olmo.attention.head_count"] = p.NumAttentionHeads
+	kv["olmo.attention.head_count_kv"] = cmp.Or(p.NumKeyValueHeads, p.NumAttentionHeads)
+
+	if p.RopeTheta > 0 {
+		kv["olmo.rope.freq_base"] = p.RopeTheta
+	} else {
+		kv["olmo.rope.freq_base"] = float32(10000.0)
+	}
+
+	if p.RMSNormEPS > 0 {
+		kv["olmo.attention.layer_norm_rms_epsilon"] = p.RMSNormEPS
+	}
+
+	if p.ClampKQV > 0 {
+		kv["olmo.attention.clamp_kqv"] = p.ClampKQV
+	}
+
+	return kv
+}
+
+func (p *olmoModel) Tensors(ts []Tensor) []*ggml.Tensor {
+	var out []*ggml.Tensor
+	for _, t := range ts {
+		out = append(out, &ggml.Tensor{
+			Name:     t.Name(),
+			Kind:     t.Kind(),
+			Shape:    t.Shape(),
+			WriterTo: t,
+		})
+	}
+
+	return out
+}
+
+func (p *olmoModel) Replacements() []string {
+	return []string{
+		"lm_head", "output",
+		"model.embed_tokens", "token_embd",
+		"model.layers", "blk",
+		"input_layernorm", "attn_norm",
+		"post_attention_layernorm", "ffn_norm",
+		"model.norm", "output_norm",
+		"self_attn.q_proj", "attn_q",
+		"self_attn.k_proj", "attn_k",
+		"self_attn.v_proj", "attn_v",
+		"self_attn.o_proj", "attn_output",
+		"mlp.gate_proj", "ffn_gate",
+		"mlp.down_proj", "ffn_down",
+		"mlp.up_proj", "ffn_up",
+	}
+}
