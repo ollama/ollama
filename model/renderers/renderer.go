b3afb5968114645a78b7d8af50aea6049fdc3a26
ParthSareen <parth.sareen@ollama.com>
2025-12-08 13:00:59 -0800

    renderers/parsers: olmo3 instruct

--- model/renderers/renderer.go
+++ model/renderers/renderer.go
@@ -59,6 +59,9 @@ func rendererForName(name string) Renderer {
 	case "cogito":
 		renderer := &CogitoRenderer{isThinking: true}
 		return renderer
+	case "olmo3":
+		renderer := &Olmo3Renderer{}
+		return renderer
 	case "olmo3-think":
 		renderer := &Olmo3ThinkRenderer{}
 		return renderer
