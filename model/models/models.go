e0da7629912bb650de4a2628e88947c605472ef6
TommyBoiss <165361500+TommyBoiss@users.noreply.github.com>
2026-01-15 14:00:55 +1300

    Run Gofmt on models.go

--- model/models/models.go
+++ model/models/models.go
@@ -8,6 +8,7 @@ import (
 	_ "github.com/ollama/ollama/model/models/gemma3"
 	_ "github.com/ollama/ollama/model/models/gemma3n"
 	_ "github.com/ollama/ollama/model/models/gptoss"
+	_ "github.com/ollama/ollama/model/models/lfm2"
 	_ "github.com/ollama/ollama/model/models/llama"
 	_ "github.com/ollama/ollama/model/models/llama4"
 	_ "github.com/ollama/ollama/model/models/mistral3"
@@ -18,5 +19,4 @@ import (
 	_ "github.com/ollama/ollama/model/models/qwen25vl"
 	_ "github.com/ollama/ollama/model/models/qwen3"
 	_ "github.com/ollama/ollama/model/models/qwen3vl"
-	_ "github.com/ollama/ollama/model/models/lfm2"
 )
