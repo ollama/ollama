74136b089d890f7860343188ff483041b1ac9677
jmorganca <jmorganca@gmail.com>
2026-01-18 21:14:17 -0800

    remove comment

--- server/sched.go
+++ server/sched.go
@@ -571,7 +571,7 @@ func (s *Scheduler) loadImageGen(req *LlmRequest) bool {
 		model:           req.model,
 		modelPath:       req.model.ModelPath,
 		llama:           server,
-		Options:         &req.opts, // Required for needsReload to not always return true
+		Options:         &req.opts,
 		loading:         false,
 		sessionDuration: sessionDuration,
 		totalSize:       server.TotalSize(),
