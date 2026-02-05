693101589a21e410e4bbd246d519f2c4f5eb7e27
Michael Yang <git@mxy.ng>
2026-02-05 10:47:21 -0800

    simplify runner selection

--- runner/runner.go
+++ runner/runner.go
@@ -11,22 +11,13 @@ func Execute(args []string) error {
 		args = args[1:]
 	}
 
-	var newRunner bool
-	var mlxRunner bool
-	if len(args) > 0 && args[0] == "--ollama-engine" {
-		args = args[1:]
-		newRunner = true
+	if len(args) > 0 {
+		switch args[0] {
+		case "--ollama-engine":
+			return ollamarunner.Execute(args[1:])
+		case "--mlx-engine":
+			return mlxrunner.Execute(args[1:])
+		}
 	}
-	if len(args) > 0 && args[0] == "--mlx-engine" {
-		args = args[1:]
-		mlxRunner = true
-	}
-
-	if mlxRunner {
-		return mlxrunner.Execute(args)
-	} else if newRunner {
-		return ollamarunner.Execute(args)
-	} else {
-		return llamarunner.Execute(args)
-	}
+	return llamarunner.Execute(args)
 }
