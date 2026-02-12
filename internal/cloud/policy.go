964fd11590e2a15d472aa518b3bf07d7ecbbddf6
Devon Rifkin <drifkin@drifkin.net>
2026-02-12 11:42:15 -0800

    Revert "Update internal/cloud/policy.go"
    This reverts commit 8bff8615f9b5751fc5c0d1273b07c9de651e07f9.
    
    Since this error shows up in other integrations, we want it to be
    prefixed with Ollama

--- internal/cloud/policy.go
+++ internal/cloud/policy.go
@@ -4,7 +4,7 @@ import (
 	"github.com/ollama/ollama/envconfig"
 )
 
-const DisabledMessagePrefix = "cloud is disabled"
+const DisabledMessagePrefix = "ollama cloud is disabled"
 
 // Status returns whether cloud is disabled and the source of the decision.
 // Source is one of: "none", "env", "config", "both".
