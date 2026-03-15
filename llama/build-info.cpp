e0dad629c5971f453cbf1a4248092fa02b986af4
inforithmics <thomas.stocker@gmail.com>
2026-03-15 21:22:55 +0100

    downgrade to b8353
    b8354 has a Performance regression on amd

--- llama/build-info.cpp
+++ llama/build-info.cpp
@@ -1,4 +1,4 @@
 int LLAMA_BUILD_NUMBER = 0;
-char const *LLAMA_COMMIT = "1a3d8edbbaba7f6e36096982c7c8a7ce11f4a7e8";
+char const *LLAMA_COMMIT = "6b10a82c00064d4ead889b09d7fae9eff6927d57";
 char const *LLAMA_COMPILER = "";
 char const *LLAMA_BUILD_TARGET = "";
