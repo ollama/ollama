0abf3aca36592419e66bffc54dfbd1c3fa2c3c23
Daniel Hiltgen <dhiltgen@users.noreply.github.com>
2026-04-13 13:04:11 -0700

    cgo: suppress deprecated warning to quiet down go build (#15438)

--- llama/llama.cpp/common/common.go
+++ llama/llama.cpp/common/common.go
@@ -1,6 +1,6 @@
 package common
 
-// #cgo CXXFLAGS: -std=c++17
+// #cgo CXXFLAGS: -std=c++17 -Wno-deprecated-declarations
 // #cgo CPPFLAGS: -I${SRCDIR}/../include -I${SRCDIR}/../vendor
 // #cgo CPPFLAGS: -I${SRCDIR}/../../../ml/backend/ggml/ggml/include
 import "C"
