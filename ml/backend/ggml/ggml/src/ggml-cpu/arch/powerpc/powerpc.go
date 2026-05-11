e1a81a770a44668c6f96b19db220b9de4b422e35
malugomesp <malu.gomes439@gmail.com>
2026-05-11 18:51:09 -0300

    update: add Power9 GPU suport for IBM AC922 with Tesla V100

---
+++ ml/backend/ggml/ggml/src/ggml-cpu/arch/powerpc/powerpc.go
@@ -0,0 +1,4 @@
+package powerpc
+// #cgo CXXFLAGS: -std=c++17
+// #cgo CPPFLAGS: -I${SRCDIR}/../.. -I${SRCDIR}/../../.. -I${SRCDIR}/../../../../include
+import "C"
---
+++ ml/backend/ggml/ggml/src/ggml-cpu/cpu_ppc64le.go
@@ -0,0 +1,2 @@
+package cpu
+import _ "github.com/ollama/ollama/ml/backend/ggml/ggml/src/ggml-cpu/arch/powerpc"
